220b13ddd9e6da363883f2ef748b8e9cf5532cf7
Ashil <ashilraj@gmail.com>
2019-04-25 17:31:15 +0530

    updated

--- resources/views/layouts/sections/root.blade.php
+++ resources/views/layouts/sections/root.blade.php
@@ -42,28 +42,26 @@
 
 <li class="treeview">
     <a href="#">
-      <i class="fa fa-user-plus"></i>
+      <i class="fa fa-users"></i>
       <span>Sub Dealer</span>
       <span class="pull-right-container">
       <i class="fa fa-angle-left pull-right"></i>
       </span>
   </a>
   <ul class="treeview-menu" style="display: none;">
-      <li><a href=""><i class="fa fa-plus"></i>Add Sub Dealer</a></li>
       <li><a href="{{url('/users')}}"><i class="fa fa-list"></i> List Sub Dealers</a></li>
   </ul>
 </li>
 
 <li class="treeview">
     <a href="#">
-      <i class="fa fa-user-plus"></i>
+      <i class="fa fa-users"></i>
       <span>User</span>
       <span class="pull-right-container">
       <i class="fa fa-angle-left pull-right"></i>
       </span>
   </a>
   <ul class="treeview-menu" style="display: none;">
-      <li><a href=""><i class="fa fa-plus"></i>Add User</a></li>
       <li><a href="{{url('/users')}}"><i class="fa fa-list"></i> List Users</a></li>
   </ul>
 </li>
--- storage/framework/views/e13e7dfa38ce34411a2e31064e33bf45358e1837.php
+++ storage/framework/views/e13e7dfa38ce34411a2e31064e33bf45358e1837.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>VST GPS</title>
+        <title>GPS ECLIPSE</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
@@ -77,7 +77,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    VST GPS
+                    GPS ECLIPSE
                 </div>
 
                 <div class="links">
