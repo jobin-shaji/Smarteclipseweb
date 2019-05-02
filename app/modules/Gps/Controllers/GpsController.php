5ec20caa65bd9bd3e2214dcdb22ca243c1e0779b
Ashil <ashilraj@gmail.com>
2019-05-02 15:12:40 +0530

    updated

--- app/modules/Gps/Controllers/GpsController.php
+++ app/modules/Gps/Controllers/GpsController.php
@@ -158,8 +158,13 @@ class GpsController extends Controller {
 
     public function addLocation(Request $request){
         $gps = GpsLocation::create([
-            'data'=> $request->data,
+            'vltdata'=> $request->data,
         ]);
+
+         return response()->json([
+            'message' => 'Success'
+        ]);
+         
     }
 
     //validation for gps creation
