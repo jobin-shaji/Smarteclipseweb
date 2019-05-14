934d58b3164b0691ff258a0723611932bfc41fd8
jisna <phpdeveloper04@vehiclest.in>
2019-05-14 16:52:45 +0530

    updated

--- app/modules/Geofence/Models/Geofence.php
+++ app/modules/Geofence/Models/Geofence.php
@@ -9,7 +9,7 @@ class Geofence extends Model
 	use SoftDeletes;    
 
 	protected $fillable=[
-		'user_id','cordinates','fence_type_id'
+		'user_id','name','fence_type_id'
 	];
 
 	protected $casts = [
