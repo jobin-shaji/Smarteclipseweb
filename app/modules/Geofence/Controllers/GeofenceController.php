4f7354efe7ef19bafe423136199d25c451d880ef
jisna <phpdeveloper04@vehiclest.in>
2019-05-16 16:18:12 +0530

    updated

--- app/modules/Geofence/Controllers/GeofenceController.php
+++ app/modules/Geofence/Controllers/GeofenceController.php
@@ -13,25 +13,25 @@ use DataTables;
 class GeofenceController extends Controller {
 
     //Display all etms
-    public function create()
+	public function create()
     {
-        return view('Geofence::fence-create');
-    }
+		return view('Geofence::fence-create');
+	}
 
-    public function saveFence(Request $request){
-        foreach ($request->polygons as $polygon) {
-            Geofence::create([
-                'user_id' => $request->user()->id,
-                'cordinates' => $polygon,
-                'fence_type_id' => 1
-            ]);
-        }
-        return response()->json([
+	public function saveFence(Request $request){
+		foreach ($request->polygons as $polygon) {
+			Geofence::create([
+				'user_id' => $request->user()->id,
+				'cordinates' => $polygon,
+				'fence_type_id' => 1
+			]);
+		}
+		return response()->json([
             'status' => 1,
             'title' => 'Success',
             'message' => 'Geofence added successfully'
         ]);
-    }
+	}
 
 
      public function geofenceListPage()
