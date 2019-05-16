f98a00951f34a8416379d52164160c0833c1fbbd
jisna <phpdeveloper04@vehiclest.in>
2019-05-16 14:44:20 +0530

    latest

--- app/modules/Vehicle/Views/vehicle-tracker.blade.php
+++ app/modules/Vehicle/Views/vehicle-tracker.blade.php
@@ -14,9 +14,75 @@
   </section>
 
   <section class="content">
-    <div style="position: absolute;">
-      <div id="map" style="position: "></div>
-    </div>
+    <div class="col-lg-12 col-sm-12">
+                      
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body">
+                              <h2 class="card-title">1</h2>
+                                
+                                <p>
+                                <b>  
+                                  </b></p><div class="cover_ofline"><b>
+                                  <span id="online_status">
+                                    <?php $online_status="M";
+                                      if($online_status=="M"){ ?>
+                                        <i class="fa fa-circle" style="color:green;" aria-hidden="true"></i> Online
+                                      <?php } elseif($online_status=="H") { ?>
+                                        <i class="fa fa-circle" style="color:yellow;" aria-hidden="true"></i> Halt
+                                      <?php } elseif($online_status=="S"){ ?>
+                                        <i class="fa fa-circle" style="color:orange;" aria-hidden="true"></i> Sleep
+                                      <?php } else{ ?>
+                                        <i class="fa fa-circle" style="color:red;" aria-hidden="true"></i> Offline
+                                      <?php } ?>
+                                  </span>
+                           
+                                 <div class="col-sm-12 social-buttons">
+                                   <a class="btn btn-block btn-social btn-bitbucket">
+                                      <i class="fa fa-car"></i><?php echo "245"; ?></a>
+                                    <a class="btn btn-block btn-social btn-bitbucket">
+                                      <i class="fa fa-key"></i> <b>
+                                        <span id="ignition">
+                                          <?php $ignition_status=1;
+                                          if($ignition_status==1){ ?>Ignition On
+                                          <?php } else{ ?> Ignition Off
+                                          <?php } ?>
+                                        </span></b>
+                                    </a>
+                                    
+                                    <a class="btn btn-block btn-social btn-bitbucket">
+                                      <i class="fa fa-tachometer"></i> <b><span id="car_speed"><?php echo 10; ?></span></b> K.M/H
+                                    </a>
+                                    <a class="btn btn-block btn-social btn-bitbucket">
+                                      <i class="fa fa-battery-full"></i><b><span id="car_bettary"><?php echo 10; ?></span></b>%
+                                    </a>
+                                    <a class="btn btn-block btn-social btn-bitbucket">
+                                      <i class="fa fa-plug"></i>
+                                      <span id="car_charging">
+                                      <?php 
+                                      $plug_Status=1; 
+                                      if($plug_Status==1){ ?>
+                                        <i class="fa fa-check"></i>
+                                      <?php }else{ ?>
+                                        <i class="fa fa-times"></i>
+                                      <?php } ?>
+                                      </span>
+                                    </a>
+                                   <div class="viewmore_location">
+                                      <i class="fa fa-map-marker"></i>-<b><span id="car_location"><?php echo "////"; ?></span></b>
+                                   </div>
+
+                                    <hr>
+
+                                    
+                                  </div>
+                                  </div>
+
+                            </div>
+                            </div> 
+
+                        <div id="map" style="width:100%;height:500px;"></div>
+
+                        </div>
   </section>
 
     @section('script')
--- public/js/gps/location-track.js
+++ public/js/gps/location-track.js
@@ -6,7 +6,92 @@
           center: {lat: 10.107570, lng: 76.345665},
           zoom: 12
         });
+        getMarkers();
       }
+
+  function getMarkers() {
+   
+        var id="<?php echo $encryptedShowID; ?>";
+        var JsonObject={id:id};
+        var old_lat=0;
+        var old_lan=0;
+          $.ajax({
+            url     :"<?php echo base_url(); ?>api/WebData/getCurrentLocationRefresh",
+            method  :"POST",
+            contentType: 'application/json',
+            context :this,
+            data    :JSON.stringify(JsonObject),
+            success : function (Result) {
+              if(Result.status=="success")
+              {
+                var vehicleData=Result.vehicleData;
+                var lat=vehicleData.latitude;
+                var lng=vehicleData.longitude;
+                var angle=vehicleData.angle;
+                var numberAngle=parseFloat(angle);
+                var ignition=vehicleData.ignition;
+                var speed=vehicleData.speed;
+                var Battery_percentage=vehicleData.Battery_percentage;
+                var mainpowerstatus=vehicleData.mainpowerstatus;
+                var location=vehicleData.location;
+                var status=vehicleData.status;
+                var colorStatus=vehicleData.colorStatus;
+                var newLatLang = new google.maps.LatLng(lat,lng);
+              
+              // marker.setPosition(newLatLang);
+                // for smooth move
+                   i = 0; 
+                   deltaLat = (lat - posLat)/numDeltas;
+                   deltaLng = (lng - posLng)/numDeltas;
+                   moveMarker();
+                 // for smooth move
+
+               map.panTo(newLatLang);
+
+               var icon = { // car icon
+                    path:vehiclePath,
+                    scale: parseFloat(vehicleScale),
+                    fillColor: colorStatus, //<-- Car Color, you can change it 
+                    fillOpacity: 1,
+                    strokeWeight: 1,
+                    anchor: new google.maps.Point(0, 5),
+                    rotation:numberAngle  //<-- Car angle
+                };
+                 
+
+              marker.setIcon(icon);
+
+              $("#car_speed").html(speed);
+              $("#car_bettary").html(Battery_percentage);
+              $("#car_location").html(location);
+
+              if(ignition==1){
+                $("#ignition").html('Ignition On');
+              }else{
+                $("#ignition").html('Ignition Off');
+              }
+
+              if(mainpowerstatus==1){
+                $("#car_charging").html('<i class="fa fa-check"></i>');
+              }else{
+                $("#car_charging").html('<i class="fa fa-times"></i>');
+              }
+
+              if(status=="M"){
+                $("#online_status").html('<i class="fa fa-circle" style="color:green;" aria-hidden="true"></i> Online');
+              }else if(status=="H"){
+                $("#online_status").html('<i class="fa fa-circle" style="color:yellow;" aria-hidden="true"></i> Halt');
+              }else if(status=="S"){
+                $("#online_status").html('<i class="fa fa-circle" style="color:orange;" aria-hidden="true"></i> Sleep');
+              }else {
+                $("#online_status").html('<i class="fa fa-circle" style="color:red;" aria-hidden="true"></i> Offline');
+              }
+              setTimeout(locate, 5000);
+            }
+
+          }
+          });
+        }
 
 
  
