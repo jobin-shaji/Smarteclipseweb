997923b4e2ec0fb3f1669dca7e88504706ca23ac
jisna <phpdeveloper04@vehiclest.in>
2019-06-21 16:29:42 +0530

    updated

--- app/modules/Reports/Views/terain-road-condition-report.blade.php
+++ app/modules/Reports/Views/terain-road-condition-report.blade.php
@@ -15,10 +15,10 @@
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
               <div class="panel-heading">
-                  <label> from Date</label>
-                  <input type="date" id="fromDate" name="fromDate">
+                   <label> from Date</label>
+                  <input type="text" class="datepicker" id="fromDate" name="fromDate">
                   <label> to date</label>
-                  <input type="date" id="toDate" name="toDate">
+                  <input type="text" class="datepicker" id="toDate" name="toDate">
                   <button class="btn btn-xs btn-info" onclick="check()"> <i class="fa fa-filter"></i> Filter </button>
                   <button class="btn btn-xs btn-info" onclick="refresh()"> <i class="fa fa-filter"></i> Refresh </button>
                  <button class="btn btn-xs btn-primary pull-right">
