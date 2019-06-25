86c4319e7be3f9d1e63788d1684750062842e48a
jisna <phpdeveloper04@vehiclest.in>
2019-06-25 09:17:43 +0530

    latest

--- resources/views/layouts/sections/eclipse-header.blade.php
+++ resources/views/layouts/sections/eclipse-header.blade.php
@@ -154,7 +154,10 @@
                                 <div class="dropdown-divider"></div>
                                 <a class="dropdown-item" href="profile.html"><i class="ti-user m-r-5 m-l-5"></i>My Profile</a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="javascript:void(0)"><i class="fa fa-power-off m-r-5 m-l-5"></i>Logout</a>
+                                <a class="btn btn-default btn-flat" href="{{ route('logout') }}"
+                    onclick="event.preventDefault();document.getElementById('logout-form').submit();">Sign out </a>
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                    onclick="event.preventDefault();document.getElementById('logout-form').submit();"><i class="fa fa-power-off m-r-5 m-l-5"></i>Logout</a>
                                 
                             </div>
                         </li>
