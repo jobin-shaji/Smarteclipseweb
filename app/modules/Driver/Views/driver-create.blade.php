f12163b7a97c23fae33bc66505b024695bd5b09f
Christeena <phpdeveloper01@vehiclest.in>
2019-06-25 19:07:35 +0530

    Sss

--- app/modules/Driver/Views/driver-create.blade.php
+++ app/modules/Driver/Views/driver-create.blade.php
@@ -12,75 +12,72 @@
     @endif  
 
 <section class="hilite-content">
-      <!-- title row -->
-        <div class="page-wrapper">
-            <!-- ============================================================== -->
-            <!-- Bread crumb and right sidebar toggle -->
-            <!-- ============================================================== -->
-            <div class="page-breadcrumb">
-                <div class="row">
-                    <div class="col-12 d-flex no-block align-items-center">
-                        <h4 class="page-title">Create Driver</h4>                      
-                    </div>
-                </div>
-            </div>           
-            <div class="container-fluid">
-              <div class="card" style="margin:0 0 0 1%">
-                    <div class="card-body wizard-content">
-                      <form  method="POST" action="{{route('driver.create.p')}}">
-                        {{csrf_field()}}
-                              <div class="card">
-                                <div class="card-body">
-                                    <h4 class="card-title"><span style="margin:0;padding:0 10px 0 0;line-height:50px"></span>DRIVER INFO</h4>
-                                    <div class="form-group row" style="float:none!important">
-                                      <label for="fname" class="col-sm-3 text-right control-label col-form-label">Name</label>
-                                        <div class="form-group has-feedback">
-                                          <input type="text" class="form-control {{ $errors->has('name') ? ' has-error' : '' }}" placeholder="Name" name="name" value="{{ old('name') }}" > 
-                                        </div>
-                                        @if ($errors->has('name'))
-                                          <span class="help-block">
-                                              <strong class="error-text">{{ $errors->first('name') }}</strong>
-                                          </span>
-                                        @endif
-                                    </div>
-
-                                    <div class="form-group row" style="float:none!important">
-                                      <label for="fname" class="col-sm-3 text-right control-label col-form-label">Address</label>
-                                        <div class="form-group has-feedback">
-                                          <input type="text" class="form-control {{ $errors->has('address') ? ' has-error' : '' }}" placeholder="Address" name="address" value="{{ old('address') }}" >
-                                        </div>
-                                      @if ($errors->has('address'))
-                                      <span class="help-block">
-                                          <strong class="error-text">{{ $errors->first('address') }}</strong>
-                                      </span>
-                                    @endif
-                                    </div>
-
-                                    <div class="form-group row" style="float:none!important">
-                                      <label for="fname" class="col-sm-3 text-right control-label col-form-label">Mobile No.</label>
-                                          <div class="form-group has-feedback">
-                                            <input type="text" class="form-control {{ $errors->has('mobile') ? ' has-error' : '' }}" placeholder="Mobile No." name="mobile" value="{{ old('mobile') }}" > 
-                                          </div>
-                                      @if ($errors->has('mobile'))
-                                        <span class="help-block">
-                                            <strong class="error-text">{{ $errors->first('mobile') }}</strong>
-                                        </span>
-                                      @endif
-                                    </div>
-                                </div>
-                              </div>
-                              <div class="border-top">
-                                  <div class="card-body">
-                                      <button type="submit" class="btn btn-primary">Submit</button>
-                                  </div>
-                              </div>
-                      </form>
-                    </div>
-                  </div>
-                    </div>
-                </div>
+  <!-- title row -->
+  <div class="page-wrapper">
+    <!-- ============================================================== -->
+    <!-- Bread crumb and right sidebar toggle -->
+    <!-- ============================================================== -->
+    <div class="page-breadcrumb">
+        <div class="row">
+            <div class="col-12 d-flex no-block align-items-center">
+                <h4 class="page-title">Create Driver</h4>                      
             </div>
         </div>
+    </div>           
+    <div class="container-fluid">
+      <div class="card" style="margin:0 0 0 1%">
+        <div class="card-body wizard-content">
+          <form  method="POST" action="{{route('driver.create.p')}}">
+            {{csrf_field()}}
+            <div class="card">
+              <div class="card-body">
+                  <h4 class="card-title"><span style="margin:0;padding:0 10px 0 0;line-height:50px"></span>DRIVER INFO</h4>
+                  <div class="form-group row" style="float:none!important">
+                    <label for="fname" class="col-sm-3 text-right control-label col-form-label">Name</label>
+                      <div class="form-group has-feedback">
+                        <input type="text" class="form-control {{ $errors->has('name') ? ' has-error' : '' }}" placeholder="Name" name="name" value="{{ old('name') }}" > 
+                      </div>
+                      @if ($errors->has('name'))
+                        <span class="help-block">
+                            <strong class="error-text">{{ $errors->first('name') }}</strong>
+                        </span>
+                      @endif
+                  </div>
 
-        </section>
+                  <div class="form-group row" style="float:none!important">
+                    <label for="fname" class="col-sm-3 text-right control-label col-form-label">Address</label>
+                      <div class="form-group has-feedback">
+                        <input type="text" class="form-control {{ $errors->has('address') ? ' has-error' : '' }}" placeholder="Address" name="address" value="{{ old('address') }}" >
+                      </div>
+                    @if ($errors->has('address'))
+                    <span class="help-block">
+                        <strong class="error-text">{{ $errors->first('address') }}</strong>
+                    </span>
+                  @endif
+                  </div>
+
+                  <div class="form-group row" style="float:none!important">
+                    <label for="fname" class="col-sm-3 text-right control-label col-form-label">Mobile No.</label>
+                        <div class="form-group has-feedback">
+                          <input type="text" class="form-control {{ $errors->has('mobile') ? ' has-error' : '' }}" placeholder="Mobile No." name="mobile" value="{{ old('mobile') }}" > 
+                        </div>
+                    @if ($errors->has('mobile'))
+                      <span class="help-block">
+                          <strong class="error-text">{{ $errors->first('mobile') }}</strong>
+                      </span>
+                    @endif
+                  </div>
+              </div>
+            </div>
+            <div class="border-top">
+                <div class="card-body">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
  @endsection
--- app/modules/Driver/Views/driver-list.blade.php
+++ app/modules/Driver/Views/driver-list.blade.php
@@ -1,58 +1,75 @@
-@extends('layouts.gps')
+@extends('layouts.eclipse')
 @section('title')
-    View Driver
+  Driver List
 @endsection
 @section('content')
-<!--  flash message -->
-@if(Session::has('message'))
-  <div class="pad margin no-print">
-    <div class="callout {{ Session::get('callout-class', 'callout-success') }}" style="margin-bottom: 0!important;">
-      {{ Session::get('message') }}  
-    </div>
+
+<!-- ============================================================== -->
+<!-- Page wrapper  -->
+<!-- ============================================================== -->
+<div class="page-wrapper">
+  <!-- ============================================================== -->
+  <!-- Bread crumb and right sidebar toggle -->
+  <!-- ============================================================== -->
+  <div class="page-breadcrumb">
+      <div class="row">
+          <div class="col-12 d-flex no-block align-items-center">
+              <h4 class="page-title">Driver List</h4>
+            
+          </div>
+      </div>
   </div>
-@endif
- <!-- end flash message -->
-<section class="content-header">
-      <h1>
-       Client 
-        <small>Control panel</small>
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active"> Driver List</li>
-      </ol>
-</section>
-<section class="content">
-  <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-              <div class="panel-heading">driver  
-                    <a href="{{route('driver.create')}}">
-                    <button class="btn btn-xs btn-primary pull-right">Add New Driver</button>
-                    </a>
-                </div>              
-                <div class="table-responsive">
-                <div class="panel-body">
-                    <table class="table table-hover table-bordered  table-striped datatable" style="width:100%" id="dataTable">
-                        <thead>
-                            <tr>
-                             <th>Sl.No</th>                              
-                              <th>Name</th>                            
-                              <th>Address</th>                              
-                              <th>Mobile</th>                            
-                             
-                              <th style="width:160px;">Action</th>
-                            </tr>
-                        </thead>
-                    </table>
-                </div>
-              </div>
+  <!-- ============================================================== -->
+  <!-- End Bread crumb and right sidebar toggle -->
+  <!-- ============================================================== -->
+  <!-- ============================================================== -->
+  <!-- Container fluid  -->
+  <!-- ============================================================== -->
+  <div class="container-fluid">
+    <!-- ============================================================== -->
+    <!-- Start Page Content -->
+    <!-- ============================================================== -->
+    <div class="card-body">
+      <div class="table-responsive">
+        <div id="zero_config_wrapper" class="dataTables_wrapper container-fluid dt-bootstrap4">                     
+          <div class="row">
+            <div class="col-sm-12">
+
+              <table class="table table-hover table-bordered  table-striped datatable" style="width:100%" id="dataTable">
+                <thead>
+                  <tr>
+                      <th>Sl.No</th>                              
+                      <th>Name</th>                            
+                      <th>Address</th>                              
+                      <th>Mobile</th>                            
+                      <th style="width:160px;">Action</th>
+                  </tr>
+                </thead>
+              </table>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-</section>
-@section('script')
-    <script src="{{asset('js/gps/driver-list.js')}}"></script>
-@endsection
+                
+  </div>
+  <!-- ============================================================== -->
+  <!-- End Container fluid  -->
+  <!-- ============================================================== -->
+  <!-- ============================================================== -->
+  <!-- footer -->
+  <!-- ============================================================== -->
+  <footer class="footer text-center">
+      All Rights Reserved by VST Mobility Solutions. Designed and Developed by <a href="https://wrappixel.com">VST</a>.
+  </footer>
+  <!-- ============================================================== -->
+  <!-- End footer -->
+  <!-- ============================================================== -->
+</div>
+
+
 @endsection
 
+  @section('script')
+    <script src="{{asset('js/gps/driver-list.js')}}"></script>
+  @endsection
