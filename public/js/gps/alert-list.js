aa9c0490c87f91bd4c048d3f7425dd8896a86b17
jisna <phpdeveloper04@vehiclest.in>
2019-06-26 17:45:59 +0530

    latest

--- public/js/gps/alert-list.js
+++ public/js/gps/alert-list.js
@@ -35,7 +35,7 @@ function callBackDataTable(){
             {data: 'alert_type.description', name: 'alert_type.description'},
             {data: 'vehicle.name', name: 'vehicle.name'},
             {data: 'vehicle.register_number', name: 'vehicle.register_number'},
-            {data: 'latitude', name: 'latitude'},
+            {data: 'address', name: 'address'},
             {data: 'created_at', name: 'created_at'},
             {data: 'action', name: 'action', orderable: false, searchable: false},           
         ],
