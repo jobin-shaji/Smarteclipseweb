872be6c2eeaa6bc358a996ffba00719e5895638e
jisna <phpdeveloper04@vehiclest.in>
2019-06-26 13:21:10 +0530

    latest

--- app/modules/Dashboard/Controllers/DashboardController.php
+++ app/modules/Dashboard/Controllers/DashboardController.php
@@ -198,14 +198,14 @@ class DashboardController extends Controller
          $satelite=$gps->satllite;
          $latitude=$gps->lat;
          $longitude=$gps->lon;
-        // if(!empty($latitude) && !empty($longitude)){
-        //     //Send request and receive json data by address
-        //     $geocodeFromLatLong = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?latlng='.trim($latitude).','.trim($longitude).'&sensor=false&key=AIzaSyDl9Ioh5neacm3nsLzjFxatLh1ac86tNgE&libraries=drawing&callback=initMap'); 
-        //     $output = json_decode($geocodeFromLatLong);         
-        //     $status = $output->status;
-        //     //Get address from json data
-        //     $address = ($status=="OK")?$output->results[1]->formatted_address:'';
-        // }
+        if(!empty($latitude) && !empty($longitude)){
+            //Send request and receive json data by address
+            $geocodeFromLatLong = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?latlng='.trim($latitude).','.trim($longitude).'&sensor=false&key=AIzaSyDl9Ioh5neacm3nsLzjFxatLh1ac86tNgE&libraries=drawing&callback=initMap'); 
+            $output = json_decode($geocodeFromLatLong);         
+            $status = $output->status;
+            //Get address from json data
+            $address = ($status=="OK")?$output->results[1]->formatted_address:'';
+        }
          $battery_status=$gps->battery_status;
         if($network_status>=50)
         {
@@ -241,7 +241,7 @@ class DashboardController extends Controller
                 'mode' => $vehcile_mode,
                 'satelite' => $satelite,
                 'battery_status' => $battery_status,
-                'address' => $latitude
+                'address' => $address
                 // 'longitude' => $gps_data->longitude
             );
         }
