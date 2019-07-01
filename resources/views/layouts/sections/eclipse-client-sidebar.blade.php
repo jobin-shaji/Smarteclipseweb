9378f14885eff10a4853a83dd1267d032bfb01a6
jisna <phpdeveloper04@vehiclest.in>
2019-07-01 17:57:12 +0530

    report-icons

--- resources/views/layouts/sections/eclipse-client-sidebar.blade.php
+++ resources/views/layouts/sections/eclipse-client-sidebar.blade.php
@@ -7,26 +7,26 @@
                    <li>
                     <a href="{{url('/daily-km-report')}}" class="animate">
                       <span class="desc animate">Daily Report </span>
-                      <span class=""><img src="/Report-icons/Total-KM-report.png" width="30px" height="30px" /></span>
+                      <span class=""><img src="{{ url('/') }}/Report-icons/Total-KM-report.png" width="30px" height="30px" /></span>
                     </a>
                   </li>
 
                   <li>
                     <a href="{{url('/geofence-report')}}" class="animate"> <span class="glyphicon glyphicon-user"></span>
                       <span class="desc animate"> Geofence Report </span>
-                      <span class=""><i class="fa fa-globe"></i></span>
+                      <span class=""><img src="{{ url('/') }}/Report-icons/geofence-report.png" width="30px" height="30px" /></span>
                     </a>
                   </li>
                   <li>
                     <a href="{{url('/alert-report')}}" class="animate">
                       <span class="desc animate"> Alert Report </span>
-                      <span class=""><i class="fa fa-warning"></i></span>
+                      <span class=""><img src="{{ url('/') }}/Report-icons/alert-report.png" width="30px" height="30px" /></span>
                     </a>
                   </li>
                   <li>
                     <a href="{{url('/tracking-report')}}" class="animate">
                       <span class="desc animate"> Tracking Report </span>
-                      <span class=""><i class="fa fa-map-pin"></i></span>
+                      <span class=""><img src="{{ url('/') }}/Report-icons/tracking-report.png" width="30px" height="30px" /></span>
                       
                     </a>
                   </li>
@@ -34,25 +34,25 @@
                   <li>
                     <a href="{{url('/route-deviation-report')}}" class="animate">
                       <span class="desc animate"> Route Deviation Report</span>
-                      <span class=""><i class="fa fa-globe"></i></span>
+                      <span class=""><img src="{{ url('/') }}/Report-icons/route-deviation-report.png" width="30px" height="30px" /></span>
                     </a>
                   </li>
                   <li>
                     <a href="{{url('/harsh-braking-report')}}" class="animate">
                       <span class="desc animate"> Harsh Braking Report</span>
-                      <span class=""><i class="fas fa-map-signs"></i></span>
+                      <span class=""><img src="{{ url('/') }}/Report-icons/braking-report.png" width="30px" height="30px" /></span>
                     </a>
                   </li>
                   <li>
                     <a href="{{url('/sudden-acceleration-report')}}" class="animate">
                       <span class="desc animate"> Sudden Acceleration Report</span>
-                      <span class=""><i class="fa fa-globe"></i></span>
+                      <span class=""><img src="{{ url('/') }}/Report-icons/accelaration-report.png" width="30px" height="30px" /></span>
                     </a>
                   </li>
                   <li>
                     <a href="{{url('/total-km-report')}}" class="animate">
                       <span class="desc animate"> Total KM  Report </span>
-                      <span class=""><i class="fas fa-tachometer-alt"></i></span>
+                      <span class=""><img src="{{ url('/') }}/Report-icons/Total-KM-report.png" width="30px" height="30px" /></span>
                     </a>
                   </li>
                   
@@ -60,38 +60,38 @@
                   <li>
                     <a href="{{url('/over-speed-report')}}" class="animate">
                       <span class="desc animate"> Over Speed  Report </span>
-                      <span class=""><i class="fas fa-car"></i></span>
+                      <span class=""><img src="{{ url('/') }}/Report-icons/overspeed-report.png" width="30px" height="30px" /></span>
                     </a>
                   </li>
                   <li>
                     <a href="{{url('/zigzag-driving-report')}}" class="animate">
                       <span class="desc animate"> Zig-Zag Driving Report</span>
-                      <span class=""><i class="fa fa-warning"></i></span>
+                      <span class=""><img src="{{ url('/') }}/Report-icons/zigzag-report.png" width="30px" height="30px" /></span>
                     </a>
                   </li>
 
                   <li>
                     <a href="{{url('/accident-imapct-alert-report')}}" class="animate">
                       <span class="desc animate"> Accident Impact Alert Report</span>
-                      <span class=""><i class="fa fa-warning"></i></span>
+                      <span class=""><img src="{{ url('/') }}/Report-icons/accident-impact-report.png" width="30px" height="30px" /></span>
                     </a>
                   </li>
                    <li>
                     <a href="{{url('/idle-report')}}" class="animate">
                       <span class="desc animate"> Idle Report</span>
-                      <span class=""><i class="fa fa-warning"></i></span>
+                      <span class=""><img src="{{ url('/') }}/Report-icons/idle-report.png" width="30px" height="30px" /></span>
                     </a>
                   </li>
                   <li>
                     <a href="{{url('/parking-report')}}" class="animate">
                       <span class="desc animate"> Parking Report</span>
-                      <span class=""><i class="fa fa-warning"></i></span>
+                      <span class=""><img src="{{ url('/') }}/Report-icons/parking-report.png" width="30px" height="30px" /></span>
                     </a>
                   </li>
                   <li>
                     <a href="{{url('/mainbattery-disconnect-report')}}" class="animate">
                       <span class="desc animate"> Main Battery Disconnect Report</span>
-                      <span class=""><i class="fa fa-warning"></i></span>
+                      <span class=""><img src="{{ url('/') }}/Report-icons/main-battery-disconnect-report.png" width="30px" height="30px" /></span>
                     </a>
                   </li>
 
