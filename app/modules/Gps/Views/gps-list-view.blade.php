1812b0a3cb3afee7ea8c10934f1ea6b93705ad3c
jisna <phpdeveloper04@vehiclest.in>
2019-07-02 10:07:06 +0530

    icons

--- app/modules/Gps/Views/gps-list-view.blade.php
+++ app/modules/Gps/Views/gps-list-view.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.eclipse')
 
 @section('content')
-<div class="page-wrapper">
+<div class="page-wrapper-new">
     <div class="page-breadcrumb">
         <div class="row">
             <div class="col-12 d-flex no-block align-items-center">
