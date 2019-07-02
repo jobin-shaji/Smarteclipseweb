675128e8a272dd9e79ed907de35ed3db1a2d04df
jisna <phpdeveloper04@vehiclest.in>
2019-07-02 14:02:08 +0530

    latest

--- app/modules/Vehicle/Views/vehicle-add.blade.php
+++ app/modules/Vehicle/Views/vehicle-add.blade.php
@@ -25,7 +25,7 @@
   </div>
  <form  method="POST" action="{{route('vehicles.create.p')}}">
         {{csrf_field()}}
-          <h4 class="card-title"><span style="margin:0;padding:0 10px 0 0;line-height:50px"><img src="{{ url('/') }}/assets/images/vehicle.png" width="40" height="40"></span>VEHICLE INFO</h4>
+         
   <div class="row">
     <div class="col-lg-6 col-md-12">
   
@@ -37,7 +37,7 @@
               <div class="row">
                 <div class="col-md-6">
                   <div class="card-body wizard-content">
-       
+        <h4 class="card-title"><span style="margin:0;padding:0 10px 0 0;line-height:50px"><img src="{{ url('/') }}/assets/images/vehicle.png" width="40" height="40"></span>VEHICLE INFO</h4>
         <div class="form-group row" style="float:none!important">
           <label for="fname" class="col-sm-3 text-right control-label col-form-label">Name</label>
           <div class="form-group has-feedback">
@@ -156,11 +156,14 @@
 
           @foreach($ota_types as $ota_type)
             <div class="col-md-5">
-              <div class="form-group has-feedback">
-                <label>{{$ota_type->name}}</label>
+              <div class="form-group row" style="float:none!important">
+          <label for="fname" class="col-sm-3 text-right control-label col-form-label">{{$ota_type->name}}</label>
+              
+                <div class="form-group has-feedback">
                 <input type="text" class="form-control {{ $errors->has('name') ? ' has-error' : '' }}" placeholder="{{$ota_type->name}}" name="ota[]" value="{{$ota_type->default_value}}" readonly> 
               </div>
             </div>
+          </div>
           @endforeach
 
         
