0ff3e6ee7d36d0834374126d35124058fcea4913
Christeena <phpdeveloper01@vehiclest.in>
2019-07-05 15:52:02 +0530

    news

--- app/modules/Vehicle/Views/location-error.blade.php
+++ app/modules/Vehicle/Views/location-error.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.eclipse') 
 @section('content')
-<div class="page-wrapper">
-	<div class="card-body">
-	    <div class="pad margin no-print">
-	      <div class="callout callout-danger" style="margin-bottom: 0!important;">
-	      No Data Reveived From GPS !!!
-	      </div>
+<div class="page-wrapper-new">
+	<div class="container-fluid">
+	    <div class="pad margin no-print"><br>
+	    	<div class="row">
+	    		<div class="col-lg-6">
+			      	<div class="callout callout-danger" style="margin-bottom: 0!important;font-size: 20px;background-color: #EFA0A0 ; padding: 5px; border: 1px;">
+			      	No Data Received From GPS !!!
+			      	</div>
+			  	</div>
+			 </div>
 	    </div>
 	</div>
 </div>
