d2b60a576448b584e34eea516b032fe3cc7c1f5f
Christeena <phpdeveloper01@vehiclest.in>
2019-07-10 16:08:26 +0530

    emergency

--- resources/views/layouts/sections/eclipse-header.blade.php
+++ resources/views/layouts/sections/eclipse-header.blade.php
@@ -167,11 +167,16 @@
                            <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark pro-pic" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="header-emergency" style="display: none;"><img src="{{ url('/') }}/assets/images/emergency.gif" alt="user" width="50"></a>
                            <div class="dropdown-menu" aria-labelledby="navbarDropdown" style="color: #FF0000">
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item">Driver : <h4 id="emergency_vehicle_driver"></h4></a>
-                                <a class="dropdown-item">Vehicle Number : <h4 id="emergency_vehicle_number"></h4></a>
-                                <a class="dropdown-item">Location : <h4 id="emergency_vehicle_location"></h4></a>
-                                <a class="dropdown-item">Time : <h4 id="emergency_vehicle_time"></h4></a>
+                                <a class="dropdown-item">Driver : <h4 id="header_emergency_vehicle_driver"></h4></a>
+                                <a class="dropdown-item">Vehicle Number : <h4 id="header_emergency_vehicle_number"></h4></a>
+                                <a class="dropdown-item">Location : <h4 id="header_emergency_vehicle_location"></h4></a>
+                                <a class="dropdown-item">Time : <h4 id="header_emergency_vehicle_time"></h4></a>
+                                <input type="hidden" id="header_em_id">
+                                <input type="hidden" id="header_alert_vehicle_id">
+                                <input type="hidden" id="header_decrypt_vehicle_id">
+                                <a class="dropdown-item"><button onclick="verifyHeaderEmergency()">Verify</button></a>
                             </div>
+
                         @role('client')
                         <li class="nav-item dropdown">
                             <a onclick="alerts()" class="nav-link dropdown-toggle waves-effect waves-dark" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="mdi mdi-bell font-24"></i>
