b2958a81814fee7473a172688234f61855e80cd2
jisna <phpdeveloper04@vehiclest.in>
2019-08-01 15:52:01 +0530

    latest

--- public/js/gps/dash-root.js
+++ public/js/gps/dash-root.js
@@ -54,8 +54,20 @@ function rootGpsUser(res){
       datasets: [{
         label: '#:',
         data:[res.dealer,res.sub_dealer,res.client],
-        backgroundColor:'rgba(242,156,18, 0.2)',
-        borderColor:'rgba(242,156,18,1)',
+
+        backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(75, 192, 192, 0.2)'
+
+                
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(75, 192, 192, 1)'
+               
+            ],
         borderWidth: 1
       }]
     },
