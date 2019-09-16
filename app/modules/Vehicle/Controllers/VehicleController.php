44e3575b3684c08121c313c26a3633c4f74b7718
Shamsu <phpdeveloper@vehiclest.in>
2019-09-16 11:28:58 +0530

    changes and update

--- app/modules/Vehicle/Controllers/VehicleController.php
+++ app/modules/Vehicle/Controllers/VehicleController.php
@@ -839,6 +839,7 @@ class VehicleController extends Controller {
         if($track_data){
 
             $plcaeName=$this->getPlacenameFromLatLng($track_data->latitude,$track_data->longitude);
+
             $snapRoute=$this->LiveSnapRoot($track_data->latitude,$track_data->longitude);
             $reponseData=array(
                         "latitude"=>$snapRoute['lat'],
@@ -1510,11 +1511,13 @@ class VehicleController extends Controller {
             //Send request and receive json data by address
             $geocodeFromLatLong = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?latlng='.trim($latitude).','.trim($longitude).'&sensor=false&key=AIzaSyAyB1CKiPIUXABe5DhoKPrVRYoY60aeigo'); 
             $output = json_decode($geocodeFromLatLong);
+             
          
             $status = $output->status;
             //Get address from json data
             $address = ($status=="OK")?$output->results[1]->formatted_address:'';
             //Return address of the given latitude and longitude
+
             if(!empty($address)){
                 return $address;
             }else{
