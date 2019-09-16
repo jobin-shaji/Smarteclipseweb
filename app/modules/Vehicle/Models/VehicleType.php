e00a2fd3a341d5d497929d9af45e959e93e8e639
Shamsu <phpdeveloper@vehiclest.in>
2019-09-16 15:09:20 +0530

    changes and update

--- app/modules/Vehicle/Models/VehicleType.php
+++ app/modules/Vehicle/Models/VehicleType.php
@@ -10,7 +10,7 @@ class VehicleType extends Model
 	public $timestamps = false;
 	
     protected $fillable = [
-        'name','status','svg_icon','vehicle_scale','opacity','strokeWeight'
+        'name','status','svg_icon','vehicle_scale','opacity','strokeWeight','online_icon','offline_icon','ideal_icon','sleep_icon'
     ];
 
 }
