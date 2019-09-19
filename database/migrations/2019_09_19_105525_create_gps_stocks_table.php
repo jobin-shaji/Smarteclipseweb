60bb0c8996f2b6d68d96d675299e35d34388f57d
Christeena <phpdeveloper01@vehiclest.in>
2019-09-19 11:03:27 +0530

    gps_stock

---
+++ database/migrations/2019_09_19_105525_create_gps_stocks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGpsStocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gps_stocks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('gps_id');
+            $table->integer('inserted_by');
+            $table->date('inserted_on');
+            $table->integer('dealer_id')->nullable();
+            $table->integer('subdealer_id')->nullable();
+            $table->integer('client_id')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gps_stocks');
+    }
+}
