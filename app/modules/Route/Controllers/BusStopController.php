628f823fb9858a32c3451fe5066ab1886a98757f
Ashil <ashilraj@gmail.com>
2019-10-08 17:32:41 +0530

    new

---
+++ app/modules/Route/Controllers/BusStopController.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Modules\Route\Controllers;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Modules\Route\Models\Route;
+use App\Modules\Vehicle\Models\VehicleType;
+use App\Modules\Vehicle\Models\Vehicle;
+use App\Modules\Client\Models\Client;
+use App\Modules\Route\Models\RouteArea;
+use App\Modules\Route\Models\RouteSchedule;
+use App\Modules\Vehicle\Models\VehicleRoute;
+use App\Modules\RouteBatch\Models\RouteBatch;
+use App\Modules\BusHelper\Models\BusHelper;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\DB;
+use DataTables;
+
+class RouteController extends Controller {
+    
+    
+}
---
+++ app/modules/Route/Models/BusStop.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Modules\Route\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class BusStop extends Model
+{
+    use SoftDeletes;
+}
