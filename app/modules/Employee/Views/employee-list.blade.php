d6c07fdb285a740425ae727256bef0525cc2fd35
christeena <developer.php02@vstmobility.com>
2019-10-11 11:35:32 +0530

    employee module

---
+++ app/modules/Employee/Views/employee-list.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.eclipse')
+@section('title')
+  Employee List
+@endsection
+@section('content')
+
+<div class="page-wrapper_new">
+  <nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-page-heading">Employee List</li>
+      <li class="breadcrumb-item active" aria-current="page"><a href="/home">Home</a>/Employee List</li>
+    </ol>  
+    @if(Session::has('message'))
+      <div class="pad margin no-print">
+        <div class="callout {{ Session::get('callout-class', 'callout-success') }}" style="margin-bottom: 0!important;">
+        {{ Session::get('message') }}  
+        </div>
+      </div>
+    @endif    
+  </nav>
+  <div class="container-fluid">
+    <div class="card-body">
+      <div class="table-responsive">
+        <div id="zero_config_wrapper" class="dataTables_wrapper container-fluid dt-bootstrap4">                     
+          <div class="row">
+            <div class="col-sm-12">
+              <table class="table table-hover table-bordered  table-striped datatable" style="width:100%" id="dataTable">
+                <thead>
+                  <tr>
+                    <th>Sl.No</th>  
+                    <th>Employee ID</th>                             
+                    <th>Name</th>
+                    <th>Contact No.</th>                                             
+                    <th>Email</th>                           
+                    <th style="width:160px;">Action</th>
+                  </tr>
+                </thead>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>         
+  </div>
+</div>
+
+@endsection
+@section('script')
+  <script src="{{asset('js/gps/employee-list.js')}}"></script>
+@endsection
