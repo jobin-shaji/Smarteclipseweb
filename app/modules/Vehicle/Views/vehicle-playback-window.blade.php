f7a959054d11444997f218c25b99018200b21277
shamsu <phpdeveloper@vehiclest.in>
2019-10-14 11:13:48 +0530

    updates in playback

--- app/modules/Vehicle/Views/vehicle-playback-window.blade.php
+++ app/modules/Vehicle/Views/vehicle-playback-window.blade.php
@@ -17,15 +17,15 @@
         });
     </script>
 
-    <script src="{{asset(palyback/assets/Scripts/jquery-3.3.1.js)}}"></script>
-    <script src="{{asset(playback/assets/Scripts/jquery-3.3.1.min.js)}}"></script>
+    <script src="{{asset('palyback/assets/Scripts/jquery-3.3.1.js')}}"></script>
+    <script src="{{asset('playback/assets/Scripts/jquery-3.3.1.min.js')}}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/2.4.0/jszip.min.js"></script>
     <!-- CSS Files -->
     <link rel="stylesheet" type="text/css" href="https://js.api.here.com/v3/3.0/mapsjs-ui.css?dp-version=1549984893" />
     <link rel="stylesheet" href="{{asset(playback/assets/css/bootstrap.min.css)}}">
     <link rel="stylesheet" href="{{asset(playback/assets/css/atlantis.min.css)}}">
     <!-- CSS Just for demo purpose, don't include it in your project -->
-    <link rel="stylesheet" href="{{asset(playback/assets/css/demo.css)}}">
+    <link rel="stylesheet" href="{{asset('playback/assets/css/demo.css')}}">
 
 </head>
 <body data-background-color="dark">
@@ -117,25 +117,25 @@
         <!-- End Custom template -->
     </div>
     <!--   Core JS Files   -->
-    <script src="{{asset(playback(playback/assets/js/core/jquery.3.2.1.min.js)}}"></script>
-    <script src="{{asset(playback/assets/js/core/popper.min.js)}}"></script>
-    <script src="{{asset(playback/assets/js/core/bootstrap.min.js)}}"></script>
+    <script src="{{asset('playback(playback/assets/js/core/jquery.3.2.1.min.js')}}"></script>
+    <script src="{{asset('playback/assets/js/core/popper.min.js')}}"></script>
+    <script src="{{asset('playback/assets/js/core/bootstrap.min.js')}}"></script>
     <!-- jQuery UI -->
-    <script src="{{asset(playback/assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js)}}"></script>
-    <script src="{{asset(playback/assets/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js)}}"></script>
+    <script src="{{asset('playback/assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js')}}"></script>
+    <script src="{{asset('playback/assets/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js')}}"></script>
     <!-- jQuery Scrollbar -->
-    <script src="{{asset(playback/assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js)}}"></script>
+    <script src="{{asset('playback/assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js')}}"></script>
 
     <!-- Chart JS -->
-    <script src="{{asset(playback/assets/js/plugin/chart.js/chart.min.js)}}"></script>
+    <script src="{{asset('playback/assets/js/plugin/chart.js/chart.min.js')}}"></script>
     <!-- jQuery Sparkline -->
-    <script src="{{asset(playback/assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js)}}"></script>
+    <script src="{{asset('playback/assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js')}}"></script>
     <!-- Chart Circle -->
-    <script src="{{asset(playback/assets/js/plugin/chart-circle/circles.min.js)}"></script>
+    <script src="{{asset('playback/assets/js/plugin/chart-circle/circles.min.js')}"></script>
     <!-- Datatables -->
-    <script src="{{asset(playback/assets/js/plugin/datatables/datatables.min.js)}}"></script>
+    <script src="{{asset('playback/assets/js/plugin/datatables/datatables.min.js')}}"></script>
     <!-- Bootstrap Notify -->
-    <script src="{{asset(playback/assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js}}"></script>
+    <script src="{{asset('playback/assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js'}}"></script>
     <!-- jQuery Vector Maps -->
     <!--<script src="../assets/js/plugin/jqvmap/jquery.vmap.min.js"></script>
     <script src="../assets/js/plugin/jqvmap/maps/jquery.vmap.world.js"></script>-->
@@ -146,11 +146,11 @@
     <script type="text/javascript" src="https://js.api.here.com/v3/3.0/mapsjs-ui.js"></script>
     <script type="text/javascript" src="https://js.api.here.com/v3/3.0/mapsjs-mapevents.js"></script>
     <!-- Sweet Alert -->
-    <script src="{{asset(playback/assets/js/plugin/sweetalert/sweetalert.min.js)}}"></script>
+    <script src="{{asset('playback/assets/js/plugin/sweetalert/sweetalert.min.js')}}"></script>
     <!-- Atlantis JS -->
-    <script src="{{asset(playback/assets/js/atlantis.min.js)}}"></script>
+    <script src="{{asset('playback/assets/js/atlantis.min.js')}}"></script>
     <!-- Atlantis DEMO methods, don't include it in your project! -->
-    <script src="{{asset(playback/assets/js/setting-demo.js)}}"></script>
+    <script src="{{'asset(playback/assets/js/setting-demo.js')}}"></script>
     <!--<script src="../assets/js/demo.js"></script>-->
     <script>
         var MapResult;
