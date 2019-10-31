fd054344a15bdc20ca4bef0aee0b2f665207c6a7
christeena <developer.php02@vstmobility.com>
2019-10-31 10:41:43 +0530

    push updates

--- public/js/gps/gps-scanner.js
+++ public/js/gps/gps-scanner.js
@@ -1,3 +1,6 @@
+function getUrl(){
+  return $('meta[name = "domain"]').attr('content');
+}
 var items = [];
 var purl = getUrl() + '/'+'gps-scan' ;
 let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
