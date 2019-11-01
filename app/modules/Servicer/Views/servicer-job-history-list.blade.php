21a5283189490fa0561ae6f8bc6e1053793bec07
christeena <developer.php02@vstmobility.com>
2019-11-01 14:31:16 +0530

    sss

--- app/modules/Servicer/Views/servicer-job-history-list.blade.php
+++ app/modules/Servicer/Views/servicer-job-history-list.blade.php
@@ -19,7 +19,7 @@
     </nav>
  
  <div class="container-fluid">
-    <div class="card-body">
+    <div class="card-body"><h4>Servicer Completed Jobs  List</h4>
       <div class="table-responsive scrollmenu">
         <div id="zero_config_wrapper" class="dataTables_wrapper container-fluid dt-bootstrap4">                     
           <div class="row">
