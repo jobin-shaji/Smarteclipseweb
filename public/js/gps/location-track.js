a994a2e437ea21a4b2d819e8875ffa2a00b66eed
christeena <developer.php02@vstmobility.com>
2019-11-06 19:05:17 +0530

    updates

--- public/js/gps/location-track.js
+++ public/js/gps/location-track.js
@@ -1,5 +1,5 @@
 function getUrl() {
-    return $('meta[name = "domain"]').attr('content');
+  return $('meta[name = "domain"]').attr('content');
 }
 capitalize = function(str1){
   return str1.charAt(0).toUpperCase() + str1.slice(1);
@@ -26,13 +26,9 @@ var map;
 var vehicleColor = "#0C2161";
 var vehicleScale = vehicle_scale;
 var angle=0;
-
-
- var clickedPointRecent=null;
- var clickedPointCurrentlatlng=null;
- var locationQueue=[];
-
-
+var clickedPointRecent=null;
+var clickedPointCurrentlatlng=null;
+var locationQueue=[];
 
 function initMap(){
   map = new google.maps.Map(document.getElementById('map'), {
@@ -43,8 +39,8 @@ function initMap(){
         zoom: 18,
         mapTypeId: 'roadmap'
 
-    });  
-     var icon = { // car icon
+  });  
+  var icon = { // car icon
         path: vehiclePath,
         scale: parseFloat(vehicleScale),
         fillColor: vehicleColor, //<-- Car Color, you can change it 
@@ -54,192 +50,176 @@ function initMap(){
         strokeWeight: parseFloat(strokeWeight),
         anchor: new google.maps.Point(0, 5),
         rotation: 0 //<-- Car angle
-    };
-    marker = new google.maps.Marker({
+  };
+  marker = new google.maps.Marker({
         map: map,
         icon: icon
-    });
-    getMarkers(map);
+  });
+  getMarkers(map);
 }
 
-
- setTimeout(getMarkers, 1000);
+setTimeout(getMarkers, 1000);
 function getMarkers() {
+  var url = 'vehicles/location-track';
+  var id = $("#vehicle_id_data").val();
+  // var id=1;
+  data = {
+      id:id
+  };
+  var purl = getUrl() + '/' + url;
+  var triangleCoords = [];
+  $.ajax({
+      type: 'POST',
+      url: purl,
+      data: data,
+      async: true,
+      headers: {
+          'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+      },
+      success: function(res) {
+      if(res.hasOwnProperty('liveData')){
+        if (res.liveData.vehicleStatus == 'M') {
+            $("#online").show();
+            $("#halt").hide();
+            $("#offline").hide();
+            $("#sleep").hide();
+            vehicleColor="#84b752";
+        } else if (res.liveData.vehicleStatus == 'H') {
+            $("#halt").show();
+            $("#online").hide();
+            $("#offline").hide();
+            $("#sleep").hide();
+            vehicleColor="#69b4b9";
 
-    var url = 'vehicles/location-track';
-    var id = $("#vehicle_id_data").val();
-    // var id=1;
-    data = {
-        id:id
-    };
-    var purl = getUrl() + '/' + url;
-    var triangleCoords = [];
-    $.ajax({
-        type: 'POST',
-        url: purl,
-        data: data,
-        async: true,
-        headers: {
-            'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
-        },
-        success: function(res) {
-
-            if(res.hasOwnProperty('liveData')){
-            // console.log(res.liveData.ign);
-                    if (res.liveData.vehicleStatus == 'M') {
-                        $("#online").show();
-                        $("#halt").hide();
-                        $("#offline").hide();
-                        $("#sleep").hide();
-                        vehicleColor="#84b752";
-                    } else if (res.liveData.vehicleStatus == 'H') {
-                        $("#halt").show();
-                        $("#online").hide();
-                        $("#offline").hide();
-                        $("#sleep").hide();
-                        vehicleColor="#69b4b9";
-
-                    } else if (res.liveData.vehicleStatus == 'S') {
-                        $("#sleep").show();
-                        $("#halt").hide();
-                        $("#online").hide();
-                        $("#offline").hide();
-                        vehicleColor=" #858585";
-                    } else {
-                      // if(res.liveData.last_seen >'1 hour ago'){
-                        
-                      // }
-                        if(res.liveData.last_seen){
-                            $('#last_seen').text(res.liveData.last_seen);
-                        }
-                        $("#offline").show();
-                        $("#sleep").hide();
-                        $("#halt").hide();
-                        $("#online").hide();
-                        vehicleColor="#c41900";
-
-                    }
-                    if (res.liveData.ign == 1) {
-                      document.getElementById("ignition").innerHTML = "Ignition ON";
-                    }else
-                    {
-                      document.getElementById("ignition").innerHTML = "Ignition OFF";
-                    }
-
-                    if(res.liveData.power == 1) {
-                      document.getElementById("car_power").innerHTML = "Connected";
-                    }else
-                    {
-                      document.getElementById("car_power").innerHTML = "Disconnected";
-                    }
-                      
-                    // document.getElementById("user").innerHTML = res.client_name;
-                    document.getElementById("vehicle_name").innerHTML = res.vehicle_reg;
-                    document.getElementById("car_speed").innerHTML = res.liveData.speed;
-                    document.getElementById("car_bettary").innerHTML = res.liveData.battery_status;
-                    document.getElementById("car_location").innerHTML = res.liveData.place;
-                    document.getElementById("user").innerHTML = res.vehicle_name.toUpperCase();
-
-                    
-                    transition(res)
-                    setTimeout(getMarkers, 10000);
-                
+        } else if (res.liveData.vehicleStatus == 'S') {
+            $("#sleep").show();
+            $("#halt").hide();
+            $("#online").hide();
+            $("#offline").hide();
+            vehicleColor=" #858585";
+        } else {
+          // if(res.liveData.last_seen >'1 hour ago'){
+            
+          // }
+            if(res.liveData.last_seen){
+                $('#last_seen').text(res.liveData.last_seen);
             }
+            $("#offline").show();
+            $("#sleep").hide();
+            $("#halt").hide();
+            $("#online").hide();
+            vehicleColor="#c41900";
 
-        },
-        error: function(err) {
-            var message = (err.responseJSON) ? err.responseJSON.message : err.responseText;
-            toastr.error(message, 'Error');
         }
-    });
+        if (res.liveData.ign == 1) {
+          document.getElementById("ignition").innerHTML = "Ignition ON";
+        }else
+        {
+          document.getElementById("ignition").innerHTML = "Ignition OFF";
+        }
 
-// --------------------------------------------------------------------
+        if(res.liveData.power == 1) {
+          document.getElementById("car_power").innerHTML = "Connected";
+        }else
+        {
+          document.getElementById("car_power").innerHTML = "Disconnected";
+        }
+          
+        // document.getElementById("user").innerHTML = res.client_name;
+        document.getElementById("vehicle_name").innerHTML = res.vehicle_reg;
+        document.getElementById("car_speed").innerHTML = res.liveData.speed;
+        document.getElementById("car_bettary").innerHTML = res.liveData.battery_status;
+        document.getElementById("car_location").innerHTML = res.liveData.place;
+        document.getElementById("user").innerHTML = res.vehicle_name.toUpperCase();
+        transition(res)
+        setTimeout(getMarkers, 10000);
+      }
 
+      },
+      error: function(err) {
+          var message = (err.responseJSON) ? err.responseJSON.message : err.responseText;
+          toastr.error(message, 'Error');
+      }
+  });
+}
 
 var snapped_points=[];
 var queueTimer=null;
 var angle=100;
 
 function drawLine(loc1,loc2)
-  {
-   var x = new google.maps.Polyline({
+{
+  var x = new google.maps.Polyline({
     path: [loc1,loc2],
     strokeColor: vehicleColor,
     strokeOpacity: 0.4,
     strokeWeight: 10
-    });
-    x.setMap(map);
-   }
+  });
+  x.setMap(map);
+}
 
 function setMarketLocation(loc,angle)
+{
+  var angle=parseFloat(angle);
+  var icon = { // car icon       
+    path:"M29.395,0H17.636c-3.117,0-5.643,3.467-5.643,6.584v34.804c0,3.116,2.526,5.644,5.643,5.644h11.759   c3.116,0,5.644-2.527,5.644-5.644V6.584C35.037,3.467,32.511,0,29.395,0z M34.05,14.188v11.665l-2.729,0.351v-4.806L34.05,14.188z    M32.618,10.773c-1.016,3.9-2.219,8.51-2.219,8.51H16.631l-2.222-8.51C14.41,10.773,23.293,7.755,32.618,10.773z M15.741,21.713   v4.492l-2.73-0.349V14.502L15.741,21.713z M13.011,37.938V27.579l2.73,0.343v8.196L13.011,37.938z M14.568,40.882l2.218-3.336   h13.771l2.219,3.336H14.568z M31.321,35.805v-7.872l2.729-0.355v10.048L31.321,35.805",
+    rotation:angle,
+    fillOpacity: 1.2,
+    scale: 0.9,
+    strokeColor: vehicleColor,
+    strokeWeight: 1
+  };
+  var icon;
+  marker.setIcon(icon);
+  var arranged_location=offsetCenter(loc)
+  marker.setPosition(arranged_location); 
+  // map.setZoom(16);
+  // map.panTo(arranged_location);      
+}
+
+function doWork()
+{
+  var current=popFromLocationQueue();
+  if(current!=null)
   {
-
-    var angle=parseFloat(angle);
-    var icon = { // car icon       
-        path:"M29.395,0H17.636c-3.117,0-5.643,3.467-5.643,6.584v34.804c0,3.116,2.526,5.644,5.643,5.644h11.759   c3.116,0,5.644-2.527,5.644-5.644V6.584C35.037,3.467,32.511,0,29.395,0z M34.05,14.188v11.665l-2.729,0.351v-4.806L34.05,14.188z    M32.618,10.773c-1.016,3.9-2.219,8.51-2.219,8.51H16.631l-2.222-8.51C14.41,10.773,23.293,7.755,32.618,10.773z M15.741,21.713   v4.492l-2.73-0.349V14.502L15.741,21.713z M13.011,37.938V27.579l2.73,0.343v8.196L13.011,37.938z M14.568,40.882l2.218-3.336   h13.771l2.219,3.336H14.568z M31.321,35.805v-7.872l2.729-0.355v10.048L31.321,35.805",
-        rotation:angle,
-        fillOpacity: 1.2,
-        scale: 0.9,
-        strokeColor: vehicleColor,
-        strokeWeight: 1
-        };
-        var icon;
-        marker.setIcon(icon);
-        var arranged_location=offsetCenter(loc)
-        marker.setPosition(arranged_location); 
-        // map.setZoom(16);
-        // map.panTo(arranged_location);      
+    if(recentPoppedLocation==null)
+    {  
+      setMarketLocation(current[0],current[1]);
+      drawLine(current[0],current[0]);            
     }
-
-    function doWork()
-    {
-      var current=popFromLocationQueue();
- 
-      if(current!=null)
-       {
-
-
-        if(recentPoppedLocation==null)
-        {  
-            setMarketLocation(current[0],current[1]);
-            drawLine(current[0],current[0]);            
-        }
-        else{   
-            drawLine(recentPoppedLocation[0],current[0]);
-            setMarketLocation(current[0],current[1]);
-         }
-        recentPoppedLocation = current[0];
+    else{   
+      drawLine(recentPoppedLocation[0],current[0]);
+      setMarketLocation(current[0],current[1]);
      }
-    else{
-     
-     }
-    setTimeout(doWork,140);
-    }
- var timerEvent=null;
- var clickedPointCurrent;
+    recentPoppedLocation = current[0];
+  }
+  else{
+  }
+  setTimeout(doWork,140);
+}
 
- var clickedPointRecentlatlng;
+var timerEvent=null;
+var clickedPointCurrent;
+var clickedPointRecentlatlng;
 //Load google map
-  function transition(result)
-    {
-      console.log(clickedPointRecent);
-     angle=result.liveData.angle;
-     clickedPointCurrent = result.liveData.latitude + ',' + result.liveData.longitude;
-     clickedPointCurrentlatlng=new google.maps.LatLng(result.liveData.latitude, result.liveData.longitude);
-      
-
-     if(clickedPointRecent==undefined || clickedPointRecent==null)
-     {
-        getSnappedPoint([clickedPointCurrent],angle)
-     }
-     else
-     { 
-      // var distance=checkDistanceBetweenTwoPoints(clickedPointRecentlatlng,clickedPointCurrentlatlng);
-      getSnappedPoint([clickedPointRecent,clickedPointCurrent],angle);       
-     }
-     clickedPointRecent = clickedPointCurrent;
-     clickedPointRecentlatlng=clickedPointCurrentlatlng;
-    }
+function transition(result)
+{
+  console.log(clickedPointRecent);
+  angle=result.liveData.angle;
+  clickedPointCurrent = result.liveData.latitude + ',' + result.liveData.longitude;
+  clickedPointCurrentlatlng=new google.maps.LatLng(result.liveData.latitude, result.liveData.longitude);
+  if(clickedPointRecent==undefined || clickedPointRecent==null)
+  {
+    getSnappedPoint([clickedPointCurrent],angle)
+  }
+  else
+  { 
+    // var distance=checkDistanceBetweenTwoPoints(clickedPointRecentlatlng,clickedPointCurrentlatlng);
+    getSnappedPoint([clickedPointRecent,clickedPointCurrent],angle);       
+  }
+  clickedPointRecent = clickedPointCurrent;
+  clickedPointRecentlatlng=clickedPointCurrentlatlng;
+}
 // -------------------it will return snappede point between two points------------------
   function getSnappedPoint(unsnappedWaypoints,angle)
    {
