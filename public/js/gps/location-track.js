1a987142bffa2a554cfddb84452b89e9467137bd
shamsu <phpdeveloper@vehiclest.in>
2019-11-08 17:40:29 +0530

    update in live track

--- public/js/gps/location-track.js
+++ public/js/gps/location-track.js
@@ -512,7 +512,8 @@ var contentString =
  '<div class="text-center mb-4" style="margin:0 0 1.5rem .5rem!important">'+
 
 
- 'Speed,</b>-/km<br><b>Vehicle Name,</b> Plate Number</p>'+
+ '<a class="btn btn-block btn-social btn-bitbucket track_item">'+
+ '<i class="fa fa-tachometer"></i> <b><label id="car_speed">0</label> km/h</b></a>'+
  '</div>';
 
 var infowindow = new google.maps.InfoWindow({
