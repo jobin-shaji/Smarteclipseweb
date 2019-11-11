a9936438ecc2a61619288d39c3da8a3be295b8c6
shamsu <phpdeveloper@vehiclest.in>
2019-11-11 19:02:33 +0530

    update in live track

--- app/modules/Vehicle/Controllers/VehicleController.php
+++ app/modules/Vehicle/Controllers/VehicleController.php
@@ -939,40 +939,36 @@ class VehicleController extends Controller {
         $offline="Offline";
         $signal_strength="Connection Lost";
     
-        $track_data=GpsData::select('latitude as latitude',
-                      'longitude as longitude',
+        $track_data=Gps::select('lat as latitude',
+                      'lon as longitude',
                       'heading as angle',
-                      'vehicle_mode as vehicleStatus',
+                      'mode as vehicleStatus',
                       'speed',
-                      'battery_percentage',
+                      'battery_status as battery_percentage',
                       'device_time as dateTime',
                       'main_power_status as power',
                       'ignition as ign',
-                      'gps_id',
+                      'id',
                       'gsm_signal_strength as signalStrength'
                     )
                     ->where('device_time', '>=',$last_update_time)
-                    ->where('gps_id',$get_vehicle->gps_id)
-                    ->where('gps_fix',1)
+                    ->where('id',$get_vehicle->gps_id)
                     ->latest('device_time')
                     ->first();
         $minutes=0;
         if($track_data == null){
-            $track_data = GpsData::select('latitude as latitude',
-                              'longitude as longitude',
+            $track_data = Gps::select('lat as latitude',
+                              'lon as longitude',
                               'heading as angle',
                               'speed',
-                              'battery_percentage',
+                              'battery_status as battery_percentage',
                               'device_time as dateTime',
                               'main_power_status as power',
                               'ignition as ign',
-                              'gps_id',
                               \DB::raw("'$signal_strength' as signalStrength"),
                               \DB::raw("'$offline' as vehicleStatus")
                               )
-                              ->where('gps_id',$get_vehicle->gps_id)
-                              ->where('gps_fix',1)
-                              ->latest('device_time')
+                              ->where('id',$get_vehicle->gps_id)
                               ->first();
            $minutes   = Carbon::createFromTimeStamp(strtotime($track_data->dateTime))->diffForHumans();
         }
@@ -981,26 +977,11 @@ class VehicleController extends Controller {
             $plcaeName=$this->getPlacenameFromLatLng($track_data->latitude,$track_data->longitude);
             $snapRoute=$this->LiveSnapRoot($track_data->latitude,$track_data->longitude);
 
-            if(floatval($track_data->angle) <= 0)
-            {
-                $h_track_data = GpsData::
-                   select('heading','gps_id','device_time')
-                        ->where('gps_id',$track_data->gps_id)
-                        ->where('heading','!=','000.00')
-                        ->whereNotNull('heading')
-                        ->latest('device_time')
-                        ->first();
-              
-                $angle=$h_track_data->heading; 
-            }
-            else
-            {
-                $angle=$track_data->angle;
-            }
+           
             $reponseData=array(
                         "latitude"=>floatval($snapRoute['lat']),
                         "longitude"=>floatval($snapRoute['lng']),
-                        "angle"=>$angle,
+                        "angle"=>$track_data->angle,
                         "vehicleStatus"=>$track_data->vehicleStatus,
                         "speed"=>round($track_data->speed),
                         "dateTime"=>$track_data->dateTime,
