c1889ac0e0196fb5cc35ca882c2ab448d1125a07
jisna <developer.php04@vstmobility.com>
2019-11-19 13:02:58 +0530

    latest

--- app/modules/GpsConfig/Routes/web.php
+++ app/modules/GpsConfig/Routes/web.php
@@ -1,6 +1,6 @@
 <?php 
 
-Route::group(['namespace' => 'App\Modules\GpsConfig\Controllers' ] , function() {
+Route::group(['middleware' => ['web','auth','role:operations'] ,'namespace' => 'App\Modules\GpsConfig\Controllers' ] , function() {
 
 Route::get('/gps-config','GpsConfigController@gpsConfigListPage')->name('gps-config');
 Route::post('/gpsconfig-list','GpsConfigController@getAllGpsConfig')->name('gpsconfig-list');
