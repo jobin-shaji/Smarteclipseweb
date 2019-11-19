de770f298ff16a3e76c37ca109969402d51c6c89
shamsu <phpdeveloper@vehiclest.in>
2019-11-19 19:27:27 +0530

    updates in playback

--- app/modules/Vehicle/Views/vehicle-playback-in-track.blade.php
+++ app/modules/Vehicle/Views/vehicle-playback-in-track.blade.php
@@ -322,16 +322,32 @@
 
         var map = new H.Map(mapContainer, maptypes.normal.map);
         //var map = new H.Map(mapContainer, maptypes.normal.map, { center: new H.geo.Point(52.11, 0.68), zoom: 5 });
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function (location) {
-                //debugger;
-                currrentlat = location.coords.latitude;
-                currentlan = location.coords.longitude;
-                map.setCenter({ lat: location.coords.latitude, lng: location.coords.longitude });
-                map.setZoom(14);
-            });
-        }
-        map.setCenter({ lat: 10.055860, lng: 76.354247 });
+        // if (navigator.geolocation) {
+        //     navigator.geolocation.getCurrentPosition(function (location) {
+        //         //debugger;
+        //         currrentlat = location.coords.latitude;
+        //         currentlan = location.coords.longitude;
+        //         map.setCenter({ lat: location.coords.latitude, lng: location.coords.longitude });
+        //         map.setZoom(14);
+        //     });
+        // }
+
+         <?php
+                $url=url()->current();
+                $rayfleet_key="rayfleet";
+                $eclipse_key="eclipse";
+                if (strpos($url, $rayfleet_key) == true) {  ?>
+                    map.setCenter({ lat: 25.254377, lng: 51.514872 });
+                <?php }else{ ?>
+                  map.setCenter({ lat: 10.055860, lng: 76.354247 });
+                <?php } 
+            ?> 
+
+
+
+       
+
+
         map.setZoom(14);
         //End of Change Log : REQ_ID-05112019_01 : Load Map position based on GeoLocation
 
