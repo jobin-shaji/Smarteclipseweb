1e12acb69c80aa17230cf6efda7ed1cb91911fa9
Ashil <developer.php03@vstmobility.com>
2019-11-21 16:38:08 +0530

    latest

--- resources/views/layouts/sections/eclipse-footer.blade.php
+++ resources/views/layouts/sections/eclipse-footer.blade.php
@@ -3,61 +3,46 @@
           $rayfleet_key="rayfleet";
           $eclipse_key="eclipse";
           if (strpos($url, $rayfleet_key) == true) {  ?>
-				 <div class="container">
-				  <div class="row">
-				    <div class="col-md-12">
+				 
 				      <footer class="footer"> 
-				        <div class="footer-bottom">
-				          <b>RAYFLEET</b> &nbsp;Version 0.1&nbsp;&nbsp;&nbsp;&nbsp; Developed by<a href="http://www.tecnositafgulf.qa" target="blank">Gulf Business Development Group</a>
+				      	<b>RAYFLEET</b> &nbsp;Version 0.1
+				        <div class="footer-bottom" style="width: 30%!important">
+				         Developed by&nbsp;<a href="http://www.tecnositafgulf.qa" target="blank">Gulf Business Development Group</a>
 				        </div>
 				      </footer>
-				    </div>
-				  </div>
-				</div>
+				   
           <?php } 
           else if (strpos($url, $eclipse_key) == true) { ?>
-                <div class="container">
-				  <div class="row">
-				    <div class="col-md-12">
-				      <footer class="footer"> 
-				        <div class="footer-bottom">
-				          Developed by <a href="http://vstmobility.com" target="blank">VST Mobility Solutions</a>&nbsp;&nbsp;&nbsp;&nbsp;<b>ECLIPSE</b> &nbsp;Version 0.1
-				        </div>
-				      </footer>
-				    </div>
-				  </div>
-				</div>
-          <?php }
+                <footer class="footer"> 
+			      	Developed by&nbsp; <a href="http://vstmobility.com" target="blank">VST Mobility Solutions</a>
+			        <div class="footer-bottom">
+			         ECLIPSE &nbsp;Version 0.1
+			        </div>
+		        </footer>
+  <?php }
           else { ?>
 
-          		<div class="container">
-				  <div class="row">
-				    <div class="col-md-12">
-				      <footer class="footer"> 
-				        <div class="footer-bottom">
-				          Developed by <a href="http://vstmobility.com" target="blank">VST Mobility Solutions</a>&nbsp;&nbsp;&nbsp;&nbsp;<b>ECLIPSE</b> &nbsp;Version 0.1
-				        </div>
-				      </footer>
-				    </div>
-				  </div>
-				</div>
-                
-
+				<footer class="footer"> 
+			      	Developed by&nbsp; <a href="http://vstmobility.com" target="blank">VST Mobility Solutions</a>
+			        <div class="footer-bottom">
+			         ECLIPSE &nbsp;Version 0.1
+			        </div>
+		        </footer>
 
    <?php } ?>
 
 <style type="text/css">
 	.footer{
-  padding: 0px 0;
-    width: 76%;
-    /*background: #dcdcdc;*/
-    color: #fff;
+  padding: 0px 4%;
+    width: 100%;
+    background: #afa9a9;
+    color: #444242;
 }
 
 .footer-bottom{
-  width: 60%;
-  text-align: center;
-  margin-left: 35%;
+  width: 13%;
+  float: right;
+  /*margin-left: 10%;*/
   color: #444242;
 }
 </style>
