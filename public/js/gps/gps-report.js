50f6a2d895840350c15690365fb26ba3b91e39f9
jisna <developer.php04@vstmobility.com>
2019-11-21 18:07:17 +0530

    gps stock

--- public/js/gps/gps-report.js
+++ public/js/gps/gps-report.js
@@ -35,10 +35,11 @@ function callBackDataTable(){
         },
         columns: [
             {data: 'DT_RowIndex', name: 'DT_Row_Index', orderable: true, searchable: false},
-            {data: 'imei', name: 'imei', orderable: false},                   
-            {data: 'serial_no', name: 'serial_no', searchable: false},
-            {data: 'e_sim_number', name: 'e_sim_number', orderable: false},
-            {data: 'manufacturing_date', name: 'manufacturing_date', orderable: false},
+            {data: 'imei', name: 'imei',  orderable: true, searchable: false},                   
+            {data: 'serial_no', name: 'serial_no',  orderable: true, searchable: false},
+            {data: 'e_sim_number', name: 'e_sim_number',  orderable: true, searchable: false},
+            {data: 'manufacturing_date', name: 'manufacturing_date', orderable: true, searchable: false},
+            
              {data: 'created_at', name: 'created_at', orderable: false, searchable: false}
            
         ],
