00c4fed984c01df8159ce14c67212ad5e8f464ad
Ashil <developer.php03@vstmobility.com>
2019-12-02 11:42:05 +0530

    latest

--- app/modules/Client/Views/dealer-client-list.blade.php
+++ app/modules/Client/Views/dealer-client-list.blade.php
@@ -9,6 +9,7 @@
     <nav aria-label="breadcrumb">
       <ol class="breadcrumb">
         <li class="breadcrumb-item active" aria-current="page"><a href="/home">Home</a>/End User List</li>
+        <h4>End User List</h4>
       </ol>
       @if(Session::has('message'))
       <div class="pad margin no-print">
@@ -19,12 +20,12 @@
       @endif       
     </nav>                
     <div class="container-fluid">             
-      <div class="card-body"><h4>End User List</h4>
+      <div class="card-body">
         <div class="table-responsive">
           <div id="zero_config_wrapper" class="dataTables_wrapper container-fluid dt-bootstrap4">          
             <div class="row">
               <div class="col-sm-12">
-                <table class="table table-hover table-bordered  table-striped datatable" style="width:100%" id="dataTable">
+                <table class="table table-hover table-bordered  table-striped datatable" style="width:100%;text-align: center;" id="dataTable">
                   <thead>
                       <tr>
                        <th>SL.No</th>                                    
