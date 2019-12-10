e329d63a4a6d78df9548005dbfcae988e43b8847
Ashil <developer.php03@vstmobility.com>
2019-12-10 17:23:52 +0530

    latest

--- app/modules/Servicer/Views/service-job-edit.blade.php
+++ app/modules/Servicer/Views/service-job-edit.blade.php
@@ -37,11 +37,11 @@
                            <input type="text" class="form-control {{ $errors->has('client') ? ' has-error' : '' }}"  name="client" value="{{$servicer_job->job_id}}" required readonly>
                             <span class="glyphicon glyphicon-phone form-control-feedback"></span>
                           </div>
-                          @if ($errors->has('client'))
+                          <!-- @if ($errors->has('client'))
                           <span class="help-block">
                           <strong class="error-text">{{ $errors->first('client') }}</strong>
                           </span>
-                          @endif
+                          @endif -->
                         </div> 
                         <div class="form-group row" style="float:none!important">
                           <label for="fname" class="col-sm-3 text-right control-label col-form-label">Client</label>
@@ -49,11 +49,7 @@
                            <input type="text" class="form-control {{ $errors->has('client') ? ' has-error' : '' }}"  name="client" value="{{$servicer_job->clients->name}}" required readonly>
                             <span class="glyphicon glyphicon-phone form-control-feedback"></span>
                           </div>
-                          @if ($errors->has('client'))
-                          <span class="help-block">
-                          <strong class="error-text">{{ $errors->first('client') }}</strong>
-                          </span>
-                          @endif
+                          
                         </div>                        
                         <div class="form-group row" style="float:none!important">
                           <label for="fname" class="col-md-5 text-right control-label col-form-label">Job Type</label>
@@ -61,11 +57,7 @@
                            <input type="text" class="form-control {{ $errors->has('job_type') ? ' has-error' : '' }}"  name="job_type" value="<?php if($servicer_job['job_type']==1){echo 'installation';} else { echo 'Services'; } ?>" required readonly>
                             <span class="glyphicon glyphicon-phone form-control-feedback"></span>
                           </div>
-                          @if ($errors->has('job_type'))
-                          <span class="help-block">
-                          <strong class="error-text">{{ $errors->first('job_type') }}</strong>
-                          </span>
-                          @endif
+                         
                         </div>
                         
                         <div class="form-group row" style="float:none!important">
@@ -74,36 +66,24 @@
                             <input type="text" class="form-control {{ $errors->has('job_date') ? ' has-error' : '' }}" placeholder="Assignee" name="job_date" value="{{$servicer_job->user->username}}" required readonly="">
                             <span class="glyphicon glyphicon-phone form-control-feedback"></span>
                           </div>
-                          @if ($errors->has('job_date'))
-                          <span class="help-block">
-                          <strong class="error-text">{{ $errors->first('job_date') }}</strong>
-                          </span>
-                          @endif
+                          
                         </div>
                          <div class="form-group row" style="float:none!important">
-                           <label for="fname" class="col-sm-5 text-right control-label col-form-label">GPS Serial No</label>
-                           <div class="form-group has-feedback">
-                             <select class="form-control selectpicker" data-live-search="true" title="Select Servicer" id="gps_id" name="gps_id" required>
-                                <option value="{{$servicer_job->gps->id}}">{{$servicer_job->gps->serial_no}}</option>
-                              </select>                           
-                           </div>
-                           @if ($errors->has('gps_id'))
-                           <span class="help-block">
-                           <strong class="error-text">{{ $errors->first('gps_id') }}</strong>
-                           </span>
-                           @endif 
-                        </div>   
+                          <label for="fname" class="col-sm-3 text-right control-label col-form-label">GPS Serial Number</label>
+                          <div class="form-group has-feedback">
+                            <input type="text" class="form-control {{ $errors->has('job_date') ? ' has-error' : '' }}" placeholder="Assignee" name="job_date" value="{{$servicer_job->gps->serial_no}}" required readonly="">
+                            <span class="glyphicon glyphicon-phone form-control-feedback"></span>
+                          </div>
+                          
+                        </div>
+
                         <div class="form-group row" style="float:none!important">               
                           <label for="fname" class="col-md-5 text-right control-label col-form-label">Description</label> 
                           <div class="form-group has-feedback">
                             <input type="text" class="form-control {{ $errors->has('description') ? ' has-error' : '' }}" placeholder="description" name="description" value="{{$servicer_job->description}}" required readonly>
                             <span class="glyphicon glyphicon-phone form-control-feedback"></span>
                           </div>
-                          @if ($errors->has('description'))
-                          <span class="help-block">
-                          <strong class="error-text">{{ $errors->first('description') }}</strong>
-                          </span>
-                          @endif
+                         
                         </div>
                         <div class="form-group row" style="float:none!important">               
                           <label for="fname" class="col-md-5 text-right control-label col-form-label">Location</label> 
@@ -111,11 +91,7 @@
                             <input type="text" class="form-control {{ $errors->has('description') ? ' has-error' : '' }}" placeholder="description" name="description" value="{{$servicer_job->location}}" required readonly>
                             <span class="glyphicon glyphicon-phone form-control-feedback"></span>
                           </div>
-                          @if ($errors->has('description'))
-                          <span class="help-block">
-                          <strong class="error-text">{{ $errors->first('description') }}</strong>
-                          </span>
-                          @endif
+                         
                         </div>
                         <div class="form-group row" style="float:none!important">
                           <label for="fname" class="col-sm-3 text-right control-label col-form-label">Job Date</label>
@@ -123,27 +99,31 @@
                             <input type="text" class="form-control {{ $errors->has('job_date') ? ' has-error' : '' }}" placeholder="Mobile" name="job_date" value=" {{date('d-m-Y', strtotime($servicer_job->job_date))}}" required readonly="">
                             <span class="glyphicon glyphicon-phone form-control-feedback"></span>
                           </div>
-                          @if ($errors->has('job_date'))
-                          <span class="help-block">
-                          <strong class="error-text">{{ $errors->first('job_date') }}</strong>
-                          </span>
-                          @endif
+                         
                         </div>
-                         <div class="form-group row" style="float:none!important">
-                           <label for="fname" class="col-sm-5 text-right control-label col-form-label">Status</label>
-                           <div class="form-group has-feedback">
-                             <select class="form-control selectpicker" data-live-search="true" title="Select Servicer" id="gps_id" name="gps_id" required>
-                                <option value="<?php if($servicer_job['status']==1){echo 'assigned';} else { echo 'pending'; } ?>"><?php if($servicer_job['status']==1){echo 'assigned';} else { echo 'pending'; } ?></option>
-                              </select>                           
-                           </div>
-                           @if ($errors->has('gps_id'))
-                           <span class="help-block">
-                           <strong class="error-text">{{ $errors->first('gps_id') }}</strong>
-                           </span>
-                           @endif 
-                        </div>   
+                        
+                        <div class="form-group row" style="float:none!important">               
+                          <label for="fname" class="col-md-5 text-right control-label col-form-label">Status</label> 
+                          <div class="form-group has-feedback">
+                            <input type="text" class="form-control {{ $errors->has('description') ? ' has-error' : '' }}" placeholder="description" name="description" value="<?php if($servicer_job['status']==1){echo 'assigned';} else { echo 'pending'; } ?>" required readonly>
+                            <span class="glyphicon glyphicon-phone form-control-feedback"></span>
+                          </div>
+                        </div>
                         <input type="hidden"   name="client_id" id="client_id" value="{{$servicer_job->clients->id}}" >
                         <input type="hidden" name="servicer_job_id" id="servicer_job_id" value="{{$servicer_job->id}}" > 
+                        <div class="form-group row" style="float:none!important">
+                      <label for="fname" class="col-md-6 text-right control-label col-form-label">Comment</label>
+                      <div class="form-group has-feedback">
+                        <textarea name="comment" id="comment" value="" class=" form-control {{ $errors->has('comment') ? ' has-error' : '' }}" required style="border:solid 1px black;"></textarea>
+                        
+                        <span class="glyphicon glyphicon-phone form-control-feedback"></span>
+                      </div>
+                      @if ($errors->has('comment'))
+                      <span class="help-block">
+                      <strong class="error-text">{{ $errors->first('comment') }}</strong>
+                      </span>
+                      @endif
+                    </div>
                     <div class="row">
                       <div class="col-md-3 ">
                         <button type="submit" class="btn btn-primary btn-md form-btn ">Job completion</button>
@@ -155,96 +135,11 @@
               </div>
             </div>
           </form>
-           <div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog" >
-    
-      <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title"></h4>
-        </div>
-        <div class="modal-body" >
-          <form  method="POST" id="form1">
-      {{csrf_field()}}
-      <div class="row">
-      <div class="col-lg-12 col-md-12">
-            <div id="zero_config_wrapper" class="container-fluid dt-bootstrap4">
-               <div class="row">
-                  <div class="col-sm-12">                    
-                     <div class="row">
-                        <div class="col-md-6">
-                           <div class="card-body_vehicle wizard-content">                             
-                              <div class="form-group row" style="float:none!important">
-                                 <label for="fname" class="col-sm-3 text-right control-label col-form-label">Name</label>
-                                 <div class="form-group has-feedback">
-                                    <input type="text" class="form-control {{ $errors->has('name') ? ' has-error' : '' }}" placeholder="Name" name="driver_name" id="driver_name" value="{{ old('name') }}" > 
-                                 </div>
-                                 @if ($errors->has('name'))
-                                  <span class="help-block">
-                                      <strong class="error-text">{{ $errors->first('name') }}</strong>
-                                  </span>
-                                @endif
-                              </div>
-                               <div class="form-group row" style="float:none!important">
-                                 <label for="fname" class="col-sm-3 text-right control-label col-form-label">Mobile</label>
-                                 <div class="form-group has-feedback">
-                                     <input type="number" class="form-control {{ $errors->has('mobile') ? ' has-error' : '' }}" placeholder="Mobile No." name="mobile" id="mobile" value="{{ old('mobile') }}" > 
-                                  </div>
-                                  @if ($errors->has('mobile'))
-                                    <span class="help-block">
-                                        <strong class="error-text">{{ $errors->first('mobile') }}</strong>
-                                    </span>
-                                  @endif
-                              </div>
-                              <div class="form-group row" style="float:none!important">
-                                 <label for="fname" class="col-sm-3 text-right control-label col-form-label">Address</label>
-                                 <div class="form-group has-feedback">
-                                    <textarea class="form-control driver_address {{ $errors->has('address') ? ' has-error' : '' }}" placeholder="Address" name="address" id="address" rows=5></textarea>
-                                  </div>
-                                   @if ($errors->has('address'))
-                                <span class="help-block">
-                                    <strong class="error-text">{{ $errors->first('address') }}</strong>
-                                    </span>
-                                @endif
-                              </div>                                                         
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-       </div>
-       <!-- <div class="row">
-         <div class="col-lg-6 col-md-12">
-            <div id="zero_config_wrapper" class="container-fluid dt-bootstrap4">
-              <div class="row">
-              
-              </div>
-            </div>
-          </div>
-        </div>  -->   
-        <div class="modal-footer">   
-          <button type="button" id="btn" class="btn btn-primary" onclick="createDriver({{$servicer_job->id}})">Create</button>
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        </div>
-     
-      </form> 
-    </div>
-         <!--  <p>Some text in the modal.</p> -->
-        </div>
-      
-      
-    </div>
-  </div>
-  
-
-          </div>
         </div>
       </div>
-    </div>            
+    </div>
   </div>
+</div>
 </div>
 <div class="clearfix"></div>
 
