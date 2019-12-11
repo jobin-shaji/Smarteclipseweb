f271ed35c64bfdca930c07c433b510c48d201755
Ashil <developer.php03@vstmobility.com>
2019-12-11 12:46:49 +0530

    latest

--- app/modules/Servicer/Views/service-job-edit.blade.php
+++ app/modules/Servicer/Views/service-job-edit.blade.php
@@ -25,7 +25,7 @@
               <div id="zero_config_wrapper" class="dataTables_wrapper container-fluid dt-bootstrap4">
 
 
-               <form  method="POST" action="{{route('servicejob.complete.save',$servicer_job->id)}}"enctype="multipart/form-data">
+               <form  method="POST" action="{{route('servicejob.complete.edit',$servicer_job->id)}}"enctype="multipart/form-data">
                 <div class="row">
                   <div class="col-md-6">      
                     {{csrf_field()}}
@@ -34,7 +34,7 @@
                         <div class="form-group row" style="float:none!important">
                           <label for="fname" class="col-sm-3 text-right control-label col-form-label">Job Code</label>
                           <div class="form-group has-feedback">
-                           <input type="text" class="form-control {{ $errors->has('client') ? ' has-error' : '' }}"  name="client" value="{{$servicer_job->job_id}}" required readonly>
+                           <input type="text" class="form-control {{ $errors->has('client') ? ' has-error' : '' }}" id="job_id" name="client" value="{{$servicer_job->job_id}}" required readonly>
                             <span class="glyphicon glyphicon-phone form-control-feedback"></span>
                           </div>
                           <!-- @if ($errors->has('client'))
@@ -46,7 +46,7 @@
                         <div class="form-group row" style="float:none!important">
                           <label for="fname" class="col-sm-3 text-right control-label col-form-label">Client</label>
                           <div class="form-group has-feedback">
-                           <input type="text" class="form-control {{ $errors->has('client') ? ' has-error' : '' }}"  name="client" value="{{$servicer_job->clients->name}}" required readonly>
+                           <input type="text" class="form-control {{ $errors->has('client') ? ' has-error' : '' }}" id="client" name="client" value="{{$servicer_job->clients->name}}" required readonly>
                             <span class="glyphicon glyphicon-phone form-control-feedback"></span>
                           </div>
                           
@@ -54,7 +54,7 @@
                         <div class="form-group row" style="float:none!important">
                           <label for="fname" class="col-md-5 text-right control-label col-form-label">Job Type</label>
                           <div class="form-group has-feedback">
-                           <input type="text" class="form-control {{ $errors->has('job_type') ? ' has-error' : '' }}"  name="job_type" value="<?php if($servicer_job['job_type']==1){echo 'installation';} else { echo 'Services'; } ?>" required readonly>
+                           <input type="text" class="form-control {{ $errors->has('job_type') ? ' has-error' : '' }}" id="job_type" name="job_type" value="<?php if($servicer_job['job_type']==1){echo 'installation';} else { echo 'Services'; } ?>" required readonly>
                             <span class="glyphicon glyphicon-phone form-control-feedback"></span>
                           </div>
                          
@@ -63,7 +63,7 @@
                         <div class="form-group row" style="float:none!important">
                           <label for="fname" class="col-sm-3 text-right control-label col-form-label">Assignee</label>
                           <div class="form-group has-feedback">
-                            <input type="text" class="form-control {{ $errors->has('job_date') ? ' has-error' : '' }}" placeholder="Assignee" name="job_date" value="{{$servicer_job->user->username}}" required readonly="">
+                            <input type="text" class="form-control {{ $errors->has('job_date') ? ' has-error' : '' }}" placeholder="Assignee" id="assignee" name="job_date" value="{{$servicer_job->user->username}}" required readonly="">
                             <span class="glyphicon glyphicon-phone form-control-feedback"></span>
                           </div>
                           
@@ -71,7 +71,7 @@
                          <div class="form-group row" style="float:none!important">
                           <label for="fname" class="col-sm-3 text-right control-label col-form-label">GPS Serial Number</label>
                           <div class="form-group has-feedback">
-                            <input type="text" class="form-control {{ $errors->has('job_date') ? ' has-error' : '' }}" placeholder="Assignee" name="job_date" value="{{$servicer_job->gps->serial_no}}" required readonly="">
+                            <input type="text" class="form-control {{ $errors->has('job_date') ? ' has-error' : '' }}" placeholder="Assignee" id="serial_no" name="job_date" value="{{$servicer_job->gps->serial_no}}" required readonly="">
                             <span class="glyphicon glyphicon-phone form-control-feedback"></span>
                           </div>
                           
@@ -80,7 +80,7 @@
                         <div class="form-group row" style="float:none!important">               
                           <label for="fname" class="col-md-5 text-right control-label col-form-label">Description</label> 
                           <div class="form-group has-feedback">
-                            <input type="text" class="form-control {{ $errors->has('description') ? ' has-error' : '' }}" placeholder="description" name="description" value="{{$servicer_job->description}}" required readonly>
+                            <input type="text" class="form-control {{ $errors->has('description') ? ' has-error' : '' }}" placeholder="description" id="description" name="description" value="{{$servicer_job->description}}" required readonly>
                             <span class="glyphicon glyphicon-phone form-control-feedback"></span>
                           </div>
                          
@@ -88,7 +88,7 @@
                         <div class="form-group row" style="float:none!important">               
                           <label for="fname" class="col-md-5 text-right control-label col-form-label">Location</label> 
                           <div class="form-group has-feedback">
-                            <input type="text" class="form-control {{ $errors->has('description') ? ' has-error' : '' }}" placeholder="description" name="description" value="{{$servicer_job->location}}" required readonly>
+                            <input type="text" class="form-control {{ $errors->has('description') ? ' has-error' : '' }}" placeholder="description" id="location" name="description" value="{{$servicer_job->location}}" required readonly>
                             <span class="glyphicon glyphicon-phone form-control-feedback"></span>
                           </div>
                          
@@ -96,7 +96,7 @@
                         <div class="form-group row" style="float:none!important">
                           <label for="fname" class="col-sm-3 text-right control-label col-form-label">Job Date</label>
                           <div class="form-group has-feedback">
-                            <input type="text" class="form-control {{ $errors->has('job_date') ? ' has-error' : '' }}" placeholder="Mobile" name="job_date" value=" {{date('d-m-Y', strtotime($servicer_job->job_date))}}" required readonly="">
+                            <input type="text" class="form-control {{ $errors->has('job_date') ? ' has-error' : '' }}" placeholder="Mobile" id="job_date" name="job_date" value=" {{date('d-m-Y', strtotime($servicer_job->job_date))}}" required readonly="">
                             <span class="glyphicon glyphicon-phone form-control-feedback"></span>
                           </div>
                          
@@ -105,7 +105,7 @@
                         <div class="form-group row" style="float:none!important">               
                           <label for="fname" class="col-md-5 text-right control-label col-form-label">Status</label> 
                           <div class="form-group has-feedback">
-                            <input type="text" class="form-control {{ $errors->has('description') ? ' has-error' : '' }}" placeholder="description" name="description" value="<?php if($servicer_job['status']==1){echo 'assigned';} else { echo 'pending'; } ?>" required readonly>
+                            <input type="text" class="form-control {{ $errors->has('description') ? ' has-error' : '' }}" placeholder="description" id="status" name="description" value="<?php if($servicer_job['status']==1){echo 'assigned';} else { echo 'pending'; } ?>" required readonly>
                             <span class="glyphicon glyphicon-phone form-control-feedback"></span>
                           </div>
                         </div>
@@ -126,12 +126,12 @@
                     </div>
                     <div class="row">
                       <div class="col-md-3 ">
-                        <button type="submit" class="btn btn-primary btn-md form-btn ">Job completion</button>
+                        <button type="submit" class="btn btn-primary btn-md form-btn ">Save</button>
                       </div>
+                     <button type="button" class="btn btn-primary btn-md form-btn" onclick="send({{$servicer_job->id}})">Job Completed</button>
                     </div>
                   </div>                
               </div>
-            
               </div>
             </div>
           </form>
@@ -146,6 +146,6 @@
 @endsection
  @section('script')
  <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
-    <script src="{{asset('js/gps/servicer-driver-create.js')}}"></script>
+    <script src="{{asset('js/gps/servicer-edit.js')}}"></script>
   
   @endsection
