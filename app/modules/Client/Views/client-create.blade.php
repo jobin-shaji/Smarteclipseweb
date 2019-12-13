558ef33fa8068ac71147f9e42c465e78d03a431e
reshma <developer.php06@vstmobility.com>
2019-12-13 17:19:02 +0530

    create end useer validation for text

--- app/modules/Client/Views/client-create.blade.php
+++ app/modules/Client/Views/client-create.blade.php
@@ -34,7 +34,8 @@
                       <div class="form-group row" style="float:none!important">
                         <label  for="fname" class="col-sm-3 text-right control-label col-form-label">Name</label> 
                         <div class="form-group has-feedback">
-                          <input type="text" class="form-control {{ $errors->has('name') ? ' has-error' : '' }}" placeholder="Name" name="name" value="{{ old('name') }}" required autocomplete="off"> 
+                          <input type="text" id="name"   class="form-control {{ $errors->has('name') ? ' has-error' : '' }}" placeholder="Name" id="name" name="name" value="{{ old('name') }}" required autocomplete="off">
+                         <p style="color:#FF0000" id="message">only characters are allowed</p>
                         </div>
                         @if ($errors->has('name'))
                           <span class="help-block">
