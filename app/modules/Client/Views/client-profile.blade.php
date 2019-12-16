e1d7a1d9827f427ba0ae2712d82b38b7566c002f
Ashil <developer.php03@vstmobility.com>
2019-12-16 12:38:21 +0530

    latest

--- app/modules/Client/Views/client-profile.blade.php
+++ app/modules/Client/Views/client-profile.blade.php
@@ -82,7 +82,8 @@
                 <div class="form-group has-feedback" style="width: 100px!important">
                   <label class="srequired" style="width: 100%!important">Upload Logo</label>
                   <div class="row">
-                    <div class="col-md-6">
+                    <div class="col-md-6" style="width: 215%!important">
+                      <p style="font-size: 11px!important;color: green">Image Dimension should be <b>150(w) X 40(h)</b> <br>Image types allowed: <b>PNG</b><br>Image size should be not more than <b>2mb</b></p>
                       <input type="file" name="logo" value="{{$client->logo }}">
                     </div>
                     <?php if(!empty($response)) { ?>
@@ -101,8 +102,8 @@
                   </div>
                 </div>
                 @if ($errors->has('logo'))
-                  <div style="width: 215%">
-                      <strong class="error-text">Dimension should be 150(w) X 40(h) <br>Image allowed:PNG<br>Image size not more than 2mb</strong>
+                  <div style="width: 141%">
+                      <strong class="error-text">{{ $errors->first('logo') }}</strong>
                   </div>
                 @endif
               </div>
