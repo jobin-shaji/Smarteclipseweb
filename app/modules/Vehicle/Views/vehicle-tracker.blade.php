0ab092da397dcca6a086259f3275b36f55134ca1
jisna <developer.php04@vstmobility.com>
2019-12-18 14:23:02 +0530

    change lastseen in track

--- app/modules/Vehicle/Views/vehicle-tracker.blade.php
+++ app/modules/Vehicle/Views/vehicle-tracker.blade.php
@@ -35,7 +35,7 @@
               <i class="fa fa-circle" style="color:#858585;" aria-hidden="true"></i> Sleep
             </span>
             <span id="offline" style="display: none;font-size: 13px;">
-              <i class="fa fa-circle" style="color:#c41900;" aria-hidden="true"></i> Offline:</br> Last seen <span id="last_seen"></span>
+              <i class="fa fa-circle" style="color:#c41900;" aria-hidden="true"></i> Offline</br> Last seen: <span id="last_seen"></span>
             </span>
 
 
