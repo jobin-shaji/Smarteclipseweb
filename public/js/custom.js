7461ff6348166b3e806bbc4b16eaaa2f5fc4f1f1
jisna <developer.php04@vstmobility.com>
2019-12-26 11:36:25 +0530

    manufacure select model

--- public/js/custom.js
+++ public/js/custom.js
@@ -1105,20 +1105,19 @@ function clientAlerts(){
     };   
     backgroundPostData(url,data,'alertNotification',{alert:false});           
 }
-
-// Display alert notifications
-function alertNotification(res)
-{
+ function alertNotification(res){
     if(res)
     {
-        // clear alert area
         $("#alert_notification").empty();
-        // display each alerts
-        for (var i = 0; i < res.alert.length; i++)
-        {
-            $("#alert_notification").append('<div class="dropdown-item" >'+res.alert[i].alert_type.description+'<br>'+res.alert[i].vehicle.register_number+'</div>');       
+        length=res.alert.length;
+
+        for (var i = 0; i < length; i++) {
+         description=res.alert[i].alert_type.description;
+
+            var alert='<div class="dropdown-item" >'+description+'</div>';  
+            $("#alert_notification").append(alert);       
         }  
-    }
+ }
 }
 
 function downloadLabel(id){
