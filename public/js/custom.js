4aa61fdb157fbd18cb0de3339cfbdfd013f5c2df
Ashil <developer.php03@vstmobility.com>
2019-12-26 10:58:31 +0530

    Vehicle number in alert list - bracket added before and after register number

--- public/js/custom.js
+++ public/js/custom.js
@@ -1109,7 +1109,7 @@ function alertNotification(res)
         // display each alerts
         for (var i = 0; i < res.alert.length; i++)
         {
-            $("#alert_notification").append('<div class="dropdown-item" >'+res.alert[i].alert_type.description+'<br>'+res.alert[i].vehicle.register_number+'</div>');       
+            $("#alert_notification").append('<div class="dropdown-item" >'+res.alert[i].alert_type.description+'<br>('+res.alert[i].vehicle.register_number+')</div>');       
         }  
     }
 }
