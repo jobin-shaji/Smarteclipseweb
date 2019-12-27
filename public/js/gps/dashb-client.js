060bc31ff0c582cdfa4a164a6130248d03bcd404
Ashil <developer.php03@vstmobility.com>
2019-12-27 11:39:13 +0530

    latest

--- public/js/gps/dashb-client.js
+++ public/js/gps/dashb-client.js
@@ -301,7 +301,11 @@ function moving(vehicle_mode) {
 
 function selectVehicleModeTrack(res) {
  deleteMarkers();
-  // cityCircle.setMap(null);
+ // remove green circle if already rendered
+ if( typeof cityCircle != 'undefined' )
+ {
+    cityCircle.setMap(null);
+ }
  flag = 0;
  vehicleTrack(res);
 
