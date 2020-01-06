ef1a1aa4f60f07f9e3b388317139fa0632af5db3
christeena <developer.php02@vstmobility.com>
2020-01-06 18:24:12 +0530

    remove client role from subscription

--- app/modules/Client/Views/client-subscription.blade.php
+++ app/modules/Client/Views/client-subscription.blade.php
@@ -36,8 +36,7 @@
                     <option value=" ">Select</option>
                     <option value="6">Fundamental</option>
                     <option value="7">superior</option>
-                    <option value="8">pro</option> 
-                    <option value="4">client</option>                   
+                    <option value="8">pro</option>                  
                   </select>
                 </div>
               </div>  
