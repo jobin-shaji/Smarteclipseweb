9683f64c70c52db9477fb13bedd393ce3e057500
jisna <developer.php04@vstmobility.com>
2020-01-21 15:12:40 +0530

    remove download icon

--- app/modules/Monitoring/Views/vehiclelist.blade.php
+++ app/modules/Monitoring/Views/vehiclelist.blade.php
@@ -126,11 +126,11 @@
                   <div class="right-sider-inner">
                   <!-- Monitoring details -->
                     <!-- Tabs -->
-                     <button class="btn btn-sm btn1 btn-primary dwnld" onclick="downloadMonitoringReport()">
+                  <!--    <button class="btn btn-sm btn1 btn-primary dwnld" onclick="downloadMonitoringReport()">
                               <i class="fa fa-file"></i>Download Excel</button>
 
                     <button class="btn btn-sm btn1 btn-primary dwnld" onclick="downloadNewMonitoringReport()">
-                              <i class="fa fa-file"></i>Download Excel New</button>
+                              <i class="fa fa-file"></i>Download Excel New</button> -->
                     <ul id="monitoring_details_tabs" class="nav nav-tabs">
                       <li class="monitoring_subtab ">
                         <a data-toggle="tab" href="#tab_content_vehicle" class="active">Vehicle</a>
