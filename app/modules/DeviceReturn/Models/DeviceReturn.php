713dab81b7a0fcfe8a21146c72c40fcc9d4475b5
christeena <developer.php02@vstmobility.com>
2020-03-11 15:36:01 +0530

    push by christy

--- app/modules/DeviceReturn/Models/DeviceReturn.php
+++ app/modules/DeviceReturn/Models/DeviceReturn.php
@@ -6,17 +6,17 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class DeviceReturn extends Model
 {
 	use SoftDeletes;    
-  protected $fillable=[
+	protected $fillable=[
 		'id','gps_id','servicer_id','client_id','type_of_issues','status','comments','created_at','updated_at','deleted_at'
 	];
 
 	public function gps()
 	{
-  	return $this->hasOne('App\Modules\Gps\Models\Gps','id','gps_id');
+		return $this->hasOne('App\Modules\Gps\Models\Gps','id','gps_id');
 	}
-    public function servicer()
+	public function servicer()
 	{
-  	return $this->hasOne('App\Modules\Servicer\Models\Servicer','id','servicer_id');
+		return $this->hasOne('App\Modules\Servicer\Models\Servicer','id','servicer_id');
 	}
     
 }
---
+++ app/modules/DeviceReturn/Models/DeviceReturnHistory.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Modules\DeviceReturn\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DeviceReturnHistory extends Model
+{
+    //
+}
