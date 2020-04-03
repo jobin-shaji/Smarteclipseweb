1a789532cbabfb2b543b6e8cf8aae5dfcdaab96d
shamsu <phpdeveloper@vehiclest.in>
2020-04-03 18:51:01 +0530

    updates in date time

--- app/modules/Servicer/Views/assign-servicer.blade.php
+++ app/modules/Servicer/Views/assign-servicer.blade.php
@@ -139,7 +139,7 @@
                     </div>
                      
                     <div class="form-group row" style="float:none!important">
-                      <label for="fname" class="col-sm-3 text-right control-label col-form-label">Job Date11</label>
+                      <label for="fname" class="col-sm-3 text-right control-label col-form-label">Job Date</label>
                       <div class="form-group has-feedback">
                         <input type="text" class=" job_date_picker  form-control {{ $errors->has('job_date') ? ' has-error' : '' }}" placeholder="Select Date" name="job_date" value="{{ old('job_date') }}" required>
                         <span class="glyphicon glyphicon-phone form-control-feedback"></span>
