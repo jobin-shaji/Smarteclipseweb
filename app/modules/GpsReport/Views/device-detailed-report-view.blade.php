c1896d04074528696ed5e48c942495da01560601
Jisna Thomas <developer.php04@vstmobility.com>
2020-06-22 16:31:26 +0530

    latest

--- app/modules/GpsReport/Views/device-detailed-report-view.blade.php
+++ app/modules/GpsReport/Views/device-detailed-report-view.blade.php
@@ -20,7 +20,7 @@
                     <span style="color:{{$gps_details->device_status}}">
                         <i class="fa fa-circle" aria-hidden="true"></i>
                     </span>
-                    IMEI : <?php ( isset($gps_details->imei) ) ? $imei = $gps_details->imei : $imei='-NA-' ?>{{$imei}} ( Serial No: <?php ( isset($gps_details->serial_no) ) ? $serial_no = $gps_details->serial_no : $serial_no='-NA-' ?>{{$serial_no}})</h4>
+                    Mode : <?php ( isset($gps_details->imei) ) ? $imei = $gps_details->imei : $imei='-NA-' ?>{{$imei}} ( Serial No: <?php ( isset($gps_details->serial_no) ) ? $serial_no = $gps_details->serial_no : $serial_no='-NA-' ?>{{$serial_no}})</h4>
                 </div><br>
                 <div class="row">
                     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12" style="word-break: break-all;">
@@ -33,7 +33,7 @@
                 <table class="table table-borderless" style='border: 50px solid transparent' >
                 <thead>
                 <tr class="success" >
-                    <td >IMEI</td>
+                    <td>Mode</td>
                     <td>Network Status</td>               
                     <td>Fuel Status</td>
                     <td>Speed</td>             
@@ -53,10 +53,35 @@
                     <td>Emergency</td>           
                 </tr>                
                 </thead>
-            </table>   
+            </table>               
         </div>
-            <!-- table section -->
-           
+        <div class="container">
+            <ul class="nav nav-pills">
+                <li class="active"><a data-toggle="pill" href="#home">Home</a></li>
+                <li><a data-toggle="pill" href="#menu1">Menu 1</a></li>
+                <li><a data-toggle="pill" href="#menu2">Menu 2</a></li>
+                <li><a data-toggle="pill" href="#menu3">Menu 3</a></li>
+            </ul> 
+            <div class="tab-content">
+                <div id="home" class="tab-pane fade in active">
+                    <h3>HOME</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+                <div id="menu1" class="tab-pane fade">
+                    <h3>Menu 1</h3>
+                    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+                <div id="menu2" class="tab-pane fade">
+                    <h3>Menu 2</h3>
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                </div>
+                <div id="menu3" class="tab-pane fade">
+                    <h3>Menu 3</h3>
+                    <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                </div>
+            </div>
+        </div>
+        <!-- table section -->           
         </div>
     </div>
 </section>
@@ -71,6 +96,8 @@ tr:hover {background-color: #D5D4D5;}
 </style>
 @section('script')
     <script src="{{asset('js/gps/device-detailed-view.js')}}"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 @endsection
 
 @endsection
