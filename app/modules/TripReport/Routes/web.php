65571514661142ccaea2de14e878945b9de623d6
Jisna Thomas <developer.php04@vstmobility.com>
2020-07-23 15:22:23 +0530

    latest

--- app/modules/TripReport/Routes/web.php
+++ app/modules/TripReport/Routes/web.php
@@ -2,7 +2,7 @@
     Route::group(['middleware' => ['web','auth','role:root'] , 'namespace' => 'App\Modules\TripReport\Controllers' ] , function() {
     Route::get('/vehicle-trip-report-config','VehicleTripReportController@getVehicleTripReportConfig')->name('vehicle-trip-report-config');
     Route::post('/end-user-plan','VehicleTripReportController@getPlanOfEndUser')->name('end-user-plan');
-    Route::post('/end-user-vehicle','VehicleTripReportController@getPlanBasedOnClient')->name('end.user.vehicle');
+    Route::post('/end-user-plan','VehicleTripReportController@getPlanBasedOnClient')->name('end.user.plan');
 	Route::post('/trip-report-subscription-create','VehicleTripReportController@saveTripReportSubscription')->name('trip.report.subscriptions.create.p');    
     Route::get('trip-report-subscription-vehicles/{id}','VehicleTripReportController@tripReportSubscriptionVehiclesList')->name('trip.report.subscription.vehicles');
     Route::post('trip-report-subscription-vehicles/{id}','VehicleTripReportController@saveVehicleSubscription')->name('vehicle.subscriptions.create.p');
