8923d8df36b53bda3b39bdbc2e4beedca6668984
Jisna Thomas <developer.php04@vstmobility.com>
2020-07-24 14:46:27 +0530

    updates

--- app/modules/Esim/Routes/web.php
+++ app/modules/Esim/Routes/web.php
@@ -7,9 +7,5 @@ Route::group(['middleware' => ['web','auth','role:root|operations'] , 'namespace
     Route::post('/esim-detail-encription','EsimController@esimDetailIdEncription')->name('esim-detail-encription');
     Route::get('/esim-sort-by-date','EsimController@getEsimDetails')->name('esim.sort.by.date');
     Route::get('/esim-activation-details-downloads','EsimController@getEsimDetails')->name('esim-activation-details-downloads');
-    
-    Route::get('/esim-activation-mail','EsimController@getEsimMail')->name('esim-activation-details-downloads');
-   
-    
 });
 
