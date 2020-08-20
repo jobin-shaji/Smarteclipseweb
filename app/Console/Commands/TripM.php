0bcf233bd2726a098b0876e2c9c10581cb5c019b
Ashil <lead.php01@vstmobility.com>
2020-08-20 10:55:47 +0530

    updated geocords to place name function in trip reports

--- app/Console/Commands/TripM.php
+++ app/Console/Commands/TripM.php
@@ -165,9 +165,9 @@ class TripM extends Command
                 $total_distance = $total_distance + $result;
 
                 $trip = [ 
-                            'start_address' => getPlacenameFromLatLng($start_lat, $start_lng),
+                            'start_address' => $this->getPlacenameFromGeoCords($start_lat, $start_lng),
                             'start_time'=> $start_time,
-                            'stop_address'  => getPlacenameFromLatLng($stop_lat, $stop_lng),
+                            'stop_address'  => $this->getPlacenameFromGeoCords($stop_lat, $stop_lng),
                             'stop_time' => $stop_time,
                             'duration'  => $this->dateTimediff($start_time, $stop_time),
                             'distance'  => $distance
@@ -427,5 +427,47 @@ class TripM extends Command
         return $string;
 
     }
+
+    /**
+     * 
+     *
+     * 
+     */
+    public function getPlacenameFromGeoCords($latitude,$longitude)
+    {  
+        // $app_id              = "RN9UIyGura2lyToc9aPg";
+        // $app_code            = "4YMdYfSTVVe1MOD_bDp_ZA";    
+        // $ch = curl_init();  
+        // curl_setopt($ch,CURLOPT_URL,'https://reverse.geocoder.api.here.com/6.2/reversegeocode.json?prox='.$latitude.'%2C'.$longitude.'%2C118&mode=retrieveAddresses&maxresults=1&gen=9&app_id='.$app_id.'&app_code='.$app_code);
+        // curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+        // //  curl_setopt($ch,CURLOPT_HEADER, false);
+        // $output=curl_exec($ch);
+        // curl_close($ch);
+        // $data=json_decode($output,true);
+        // return $data['Response']['View'][0]['Result'][0]['Location']['Address']['Label'];
+        if(!empty($latitude) && !empty($longitude)){
+            //Send request and receive json data by address
+            try
+            {
+                    $geocodeFromLatLong = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?latlng='.trim($latitude).','.trim($longitude).'&sensor=false&key='.config('eclipse.keys.googleMap'));
+                    $output = json_decode($geocodeFromLatLong);
+                    $status = $output->status;
+                    $address = ($status=="OK")?$output->results[0]->formatted_address:'';
+                    if(!empty($address)){
+                        $address = removeUrlFromString($address);
+                        return $address;
+                    }else{
+                        return false;
+                    }
+            }
+            catch(Exception $e)
+            {
+                report($e);
+            }
+       
+        }else{
+            return false;
+        }
+    }  
 
 }
